6f727ce2f4179dfb4befca16cb07f6c61c15c038
Till Adam <till.adam@kdab.com>
2006-04-16 07:47:51 +0000

    Add unit test that makes sure that when legends, headers or footers, and coordinate planes are deleted, the chart that owns them is notified and removes them from the respective lists. (Just like QObject parenting)
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@204 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ qtests/ChartElementOwnership/main.cpp
@@ -0,0 +1,64 @@
+#include <QtTest/QtTest>
+
+#include <KDChartChart>
+#include <KDChartGlobal>
+#include <KDChartBarDiagram>
+#include <KDChartLineDiagram>
+#include <KDChartCartesianCoordinatePlane>
+#include <KDChartLegend>
+#include <KDChartHeaderFooter>
+
+using namespace KDChart;
+
+class TestChartElementOwnership: public QObject {
+  Q_OBJECT
+private slots:
+
+  void initTestCase()
+  {
+      m_chart = new Chart(0);
+  }
+
+  void testPlaneOwnership()
+  {
+      QCOMPARE( m_chart->coordinatePlanes().size(), 1 );
+      CartesianCoordinatePlane *p = new CartesianCoordinatePlane();
+      m_chart->addCoordinatePlane( p );
+      QCOMPARE( m_chart->coordinatePlanes().size(), 2 );
+      delete p;
+      QCOMPARE( m_chart->coordinatePlanes().size(), 1 );
+  }
+
+  void testLegendOwnership()
+  {
+      QCOMPARE( m_chart->legends().size(), 0 );
+      Legend * legend = new Legend( m_chart->coordinatePlane()->diagram() );
+      m_chart->addLegend( legend );
+      QCOMPARE( m_chart->legends().size(), 1 );
+      delete legend;
+      QCOMPARE( m_chart->legends().size(), 0 );
+  }
+
+  void testHeaderFooterOwnership()
+  {
+      QCOMPARE( m_chart->headerFooters().size(), 0 );
+      HeaderFooter * h = new HeaderFooter();
+      m_chart->addHeaderFooter( h );
+      QCOMPARE( m_chart->headerFooters().size(), 1 );
+      delete h;
+      QCOMPARE( m_chart->headerFooters().size(), 0 );
+  }
+
+  
+  void cleanupTestCase()
+  {
+  }
+
+private:
+  Chart *m_chart;
+
+};
+
+QTEST_MAIN(TestChartElementOwnership)
+
+#include "main.moc"
