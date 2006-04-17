2519f8cdeac60a8f855036d811de237ec75e63d7
Karl-Heinz Zimmer <khz@klaralvdalens-datakonsult.se>
2006-04-17 09:30:03 +0000

    animate the area highlighting
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@236 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/Lines/mainwindow.cpp
+++ examples/Lines/mainwindow.cpp
@@ -10,6 +10,7 @@
 
 #include <QDebug>
 #include <QPainter>
+#include <QTimer>
 
 using namespace KDChart;
 
@@ -17,6 +18,9 @@ MainWindow::MainWindow( QWidget* parent ) :
     QWidget( parent )
 {
     setupUi( this );
+
+    m_curColumn = -1;
+    m_curOpacity = 0;
 
     QHBoxLayout* chartLayout = new QHBoxLayout( chartFrame );
     m_chart = new Chart();
@@ -28,6 +32,11 @@ MainWindow::MainWindow( QWidget* parent ) :
     m_lines = new LineDiagram();
     m_lines->setModel( &m_model );
     m_chart->coordinatePlane()->replaceDiagram( m_lines );
+
+    // Instantiate the timer
+    QTimer *timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(slot_timerFired()));
+    timer->start(40);
 }
 
 
@@ -77,19 +86,50 @@ void MainWindow::on_paintValuesCB_toggled( bool checked )
 
 void MainWindow::on_animateAreasCB_toggled( bool checked )
 {
+    if( checked )
+        highlightAreaCB->setCheckState( Qt::Unchecked );
+    highlightAreaCB->setEnabled( ! checked );
+    highlightAreaSB->setEnabled( ! checked );
+    // un-highlight all previously highlighted columns
+    const int colCount = m_lines->model()->columnCount();
+    for ( int i = 0; i<colCount; ++i )
+        setHighlightArea( i, 127, false, false );
+    m_chart->update();
+    m_curColumn = 0;
+    m_curOpacity = 0;
 }
 
-void MainWindow::on_highlightAreaCB_toggled( bool checked )
+void MainWindow::slot_timerFired()
 {
-    const int column = highlightAreaSB->value();
+    if( m_curColumn < 0 ) return;
+    m_curOpacity += 5;
+    if( m_curOpacity > 255 ){
+        setHighlightArea( m_curColumn, 127, false, false );
+        m_curOpacity = 0;
+        ++m_curColumn;
+        if( m_curColumn >= m_lines->model()->columnCount() )
+            m_curColumn = 0;
+    }
+    setHighlightArea( m_curColumn, m_curOpacity, true, true );
+}
+
+void MainWindow::setHighlightArea( int column, int opacity, bool checked, bool doUpdate )
+{
     LineAttributes la = m_lines->lineAttributes( m_lines->model()->index( 0, column, QModelIndex() ) );
     if ( checked ) {
         la.setDisplayArea( true );
+        la.setTransparency( opacity );
     }  else {
         la.setDisplayArea( false );
     }
     m_lines->setLineAttributes( column, la );
-    m_chart->update();
+    if( doUpdate )
+        m_chart->update();
+}
+
+void MainWindow::on_highlightAreaCB_toggled( bool checked )
+{
+    setHighlightArea( highlightAreaSB->value(), 127, checked, true );
 }
 
 void MainWindow::on_highlightAreaSB_valueChanged( int /*i*/ )
