43156a326ef6e399ea0c1a35dcdfa9060a5f0b95
Till Adam <till.adam@kdab.com>
2006-05-03 10:28:32 +0000

    And make it compile.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@397 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/MultidimensionalData/mainwindow.cpp
+++ examples/MultidimensionalData/mainwindow.cpp
@@ -31,6 +31,7 @@
 
 #include <QDebug>
 #include <QPainter>
+#include <QHBoxLayout>
 
 using namespace KDChart;
 
--- examples/MultidimensionalData/mainwindow.h
+++ examples/MultidimensionalData/mainwindow.h
@@ -26,7 +26,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include "ui_mainwindow.h"
+#include <QWidget>
 #include <TableModel.h>
 
 namespace KDChart {
@@ -44,6 +44,7 @@ class MainWindow : public QWidget
 
 public:
     MainWindow( QWidget* parent = 0 );
+    virtual ~MainWindow() { }
 
 private:
     KDChart::Chart* m_chart;
