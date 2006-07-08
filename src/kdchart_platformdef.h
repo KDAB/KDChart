43f4ff6bdc77bd79ca2bc233a612ba1fb155d36a
Karl-Heinz Zimmer <khz@klaralvdalens-datakonsult.se>
2006-07-08 12:44:18 +0000

    Use this internal file for platform definitions and/or methods that are to be included into our internal implementation files only.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@1269 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ src/kdchart_platformdef.h
@@ -0,0 +1,63 @@
+/****************************************************************************
+ ** Copyright (C) 2006 Klarälvdalens Datakonsult AB.  All rights reserved.
+ **
+ ** This file is part of the KD Chart library.
+ **
+ ** This file may be distributed and/or modified under the terms of the
+ ** GNU General Public License version 2 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.GPL included in the
+ ** packaging of this file.
+ **
+ ** Licensees holding valid commercial KD Chart licenses may use this file in
+ ** accordance with the KD Chart Commercial License Agreement provided with
+ ** the Software.
+ **
+ ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ **
+ ** See http://www.kdab.net/kdchart for
+ **   information about KDChart Commercial License Agreements.
+ **
+ ** Contact info@kdab.net if any conditions of this
+ ** licensing are not clear to you.
+ **
+ **********************************************************************/
+
+#ifndef KDCHART_PLATFORMDEF_H
+#define KDCHART_PLATFORMDEF_H
+
+#include <cmath>
+
+#include <qglobal.h>
+
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+#define DEGTORAD(d) (d)*M_PI/180
+
+
+// We use our own ISNAN / ISINF in the code to detect
+// that we defined them.
+// reason: Windows / MacOS do not have isnan() / isinf()
+#if defined Q_OS_WIN
+#include <float.h>
+#define ISNAN(x ) _isnan(x )
+#define ISINF(x ) (!(_finite(x ) + _isnan(x ) ) )
+#elif defined Q_OS_DARWIN
+#define ISNAN(x) std::isnan(x)
+#define ISINF(x) std::isinf(x)
+#else
+#define ISNAN(x) isnan(x)
+#define ISINF(x) isinf(x)
+#endif
+
+
+// We wrap every if() by extra { } to work around
+// the scope bug for loop counters in some compilers.
+#define for if (0) {} else for
+
+
+#endif
