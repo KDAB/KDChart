0de8d7cb7dc2c19833b3906a9f97da46d1df8552
Michel Boyer de la Giroday <michel@kdab.com>
2006-08-02 06:54:04 +0000

    example for a line chart - used by the manual
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@1675 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/Lines/Lines.pro
+++ examples/Lines/Lines.pro
@@ -1,4 +1,5 @@
 TEMPLATE	= subdirs
 SUBDIRS		= SimpleLineWidget \                  
-	          Advanced
+	          Advanced \
+	          Parameters
 
---
+++ examples/Lines/Parameters/main.cpp
@@ -0,0 +1,77 @@
+#include <QtGui>
+#include <KDChartChart>
+#include <KDChartLineDiagram>
+#include <KDChartDataValueAttributes>
+
+using namespace KDChart;
+
+class ChartWidget : public QWidget {
+  Q_OBJECT
+public:
+  explicit ChartWidget(QWidget* parent=0)
+    : QWidget(parent)
+  {
+
+      //QModelIndex mindex = QModelIndex();
+    m_model.insertRows( 0,5, QModelIndex() );
+    m_model.insertColumns( 0,5, QModelIndex() );
+
+    for( int i = 0; i < 5; ++i ) {
+      for( int j = 0; j < 5; ++j ) {
+	m_model.setData( m_model.index( i,j,QModelIndex() ), (double)i*j );
+      }
+    }
+    LineDiagram* diagram = new LineDiagram;
+    diagram->setModel(&m_model);
+
+    // Display values
+    // 1 - Call the relevant attributes
+    DataValueAttributes dva( diagram->dataValueAttributes() );
+    // 2 - We want to configure the font and colors
+    //     for the data values text.
+    TextAttributes ta( dva.textAttributes() );
+    //rotate if you wish
+    //ta.setRotation( 0 );
+    // 3 - Set up your text attributes
+    ta.setFont( QFont( "Comic", 6 ) );
+    ta .setPen( QPen( QColor( Qt::darkGreen ) ) );
+    ta.setVisible( true );
+    // 4 - Assign the text attributes to your
+    //     DataValuesAttributes
+    dva.setTextAttributes( ta );
+    dva.setVisible( true );
+    // 5 - Assign to the diagram
+    diagram->setDataValueAttributes( dva );
+
+    // Draw a the section of a line differently.
+    // 1 - Retrieve the pen for the dataset and change
+    //    its style.
+    //    This allow us to keep the line original color.
+    QPen linePen(  diagram->pen( 1 ) );
+    linePen.setWidth( 3 );
+    linePen.setStyle( Qt::DashLine );
+    // 2 - Change the Pen for a section within a line
+    diagram->setPen( m_model.index( 1, 1, QModelIndex() ), linePen );
+    // 3 - Assign to the diagram
+    m_chart.coordinatePlane()->replaceDiagram(diagram);
+
+    QVBoxLayout* l = new QVBoxLayout(this);
+    l->addWidget(&m_chart);
+    setLayout(l);
+  }
+
+private:
+  Chart m_chart;
+  QStandardItemModel m_model;
+};
+
+int main( int argc, char** argv ) {
+    QApplication app( argc, argv );
+
+    ChartWidget w;
+    w.show();
+
+    return app.exec();
+}
+
+#include "main.moc"
