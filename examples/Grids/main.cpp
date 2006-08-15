527c1fa8f9a3743614de64a7e48bcf21b2e4a78f
Michel Boyer de la Giroday <michel@kdab.com>
2006-08-15 08:48:13 +0000

    grid configuration example
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@1927 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ examples/Grids/main.cpp
@@ -0,0 +1,121 @@
+#include <QtGui>
+#include <KDChartChart>
+#include <KDChartLineDiagram>
+#include <KDChartHeaderFooter>
+#include <KDChartPosition>
+#include <KDChartBackgroundAttributes>
+#include <KDChartFrameAttributes>
+#include <KDChartGridAttributes>
+#include <KDChartAbstractCoordinatePlane>
+#include <KDChartCartesianCoordinatePlane>
+#include <KDChartAbstractCartesianDiagram>
+
+
+using namespace KDChart;
+
+class ChartWidget : public QWidget {
+  Q_OBJECT
+public:
+  explicit ChartWidget(QWidget* parent=0)
+    : QWidget(parent)
+  {
+
+    m_model.insertRows( 0, 2, QModelIndex() );
+    m_model.insertColumns(  0,  3,  QModelIndex() );
+    for (int row = 0; row < 3; ++row) {
+            for (int column = 0; column < 3; ++column) {
+                QModelIndex index = m_model.index(row, column, QModelIndex());
+                m_model.setData(index, QVariant(row+1 * column) );
+            }
+    }
+
+    LineDiagram* diagram = new LineDiagram;
+    diagram->setModel(&m_model);
+
+    m_chart.coordinatePlane()->replaceDiagram(diagram);
+
+    // Add at one Header and set it up
+    HeaderFooter* header = new HeaderFooter( &m_chart );
+    header->setPosition( Position::North );
+    header->setText( "A Line Chart with Grid Configured" );
+    m_chart.addHeaderFooter( header );
+
+    // Configure the Header text attributes
+    TextAttributes hta;
+    hta.setPen( QPen(  Qt::red ) );
+
+    // let the header resize itself
+    // together with the widget.
+    // so-called relative size
+    Measure m( 35.0 );
+    m.setRelativeMode( header->autoReferenceArea(),
+                       KDChartEnums::MeasureOrientationMinimum );
+    hta.setFontSize( m );
+    // min font size
+    m.setValue( 3.0 );
+    m.setCalculationMode( KDChartEnums::MeasureCalculationModeAbsolute );
+    hta.setMinimalFontSize( m  );
+    header->setTextAttributes( hta );
+
+    // Configure the Header's Background
+    BackgroundAttributes hba;
+    hba.setBrush( Qt::white );
+    hba.setVisible( true );
+    header->setBackgroundAttributes(  hba );
+
+    // Configure the header Frame attributes
+    FrameAttributes hfa;
+    hfa.setPen( QPen ( QBrush( Qt::darkGray ), 2 ) );
+    hfa.setPadding( 2 );
+    hfa.setVisible( true );
+    header->setFrameAttributes(  hfa );
+
+    // diagram->coordinatePlane returns an abstract plane one.
+    // if we want to specify the orientation we need to cast
+    // as follow
+    CartesianCoordinatePlane* plane = static_cast <CartesianCoordinatePlane*>
+		    ( diagram->coordinatePlane() );
+
+    // retrieve your grid attributes
+    // display grid and sub-grid
+    GridAttributes ga ( plane->gridAttributes( Qt::Vertical ) );
+    ga.setGridVisible(  true );
+    ga.setSubGridVisible( true );
+
+    // Configure a grid pen
+    QPen gridPen(  Qt::magenta );
+    gridPen.setWidth( 3 );
+    ga.setGridPen(  gridPen );
+
+    // Configure a sub-grid pen
+    QPen subGridPen( Qt::darkGray );
+    subGridPen.setStyle( Qt::DotLine );
+    ga.setSubGridPen(  subGridPen );
+
+    // Display a blue zero line
+    ga.setZeroLinePen( QPen( Qt::blue ) );
+
+    // Assign your grid to the plane
+    plane->setGridAttributes( Qt::Vertical,  ga );
+
+    QVBoxLayout* l = new QVBoxLayout(this);
+    l->addWidget(&m_chart);
+    setLayout(l);
+  }
+
+private:
+  Chart m_chart;
+  QStandardItemModel m_model;
+    QPixmap pixmap;
+};
+
+int main( int argc, char** argv ) {
+    QApplication app( argc, argv );
+
+    ChartWidget w;
+    w.show();
+
+    return app.exec();
+}
+
+#include "main.moc"
--- examples/examples.pro
+++ examples/examples.pro
@@ -14,6 +14,7 @@ SUBDIRS		= tools \
                   MultidimensionalData \
                   EmptyValues \
 	          Charts \
-	          Background
+	          Background \
+	          Grids
 
 
