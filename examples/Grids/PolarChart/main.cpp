83603126f4bc9ee38c4db965e0366475cc434c4f
Christoph Schleifenbaum <christoph.schleifenbaum@kdab.com>
2006-09-28 12:40:23 +0000

    added a simple example for polar diagram using the Chart class
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@2642 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ examples/Grids/PolarChart/main.cpp
@@ -0,0 +1,41 @@
+#include <KDChartChart>
+#include <KDChartPolarDiagram>
+#include <KDChartPolarCoordinatePlane>
+
+#include <QApplication>
+#include <QStandardItemModel>
+
+using namespace KDChart;
+
+int main( int argc, char** argv ) {
+    QApplication app( argc, argv );
+
+    // creating the chart
+    Chart chart;
+
+    // the model
+    QStandardItemModel model;
+    model.insertRows( 0, 50 );
+    model.insertColumns(  0,  5 );
+    int i = 0;
+
+    // fill it with some nice looking data
+    for ( int column = 0; column <= model.columnCount(); ++column ) {
+        for ( int row = 0; row <= model.rowCount(); ++row ) {
+            QModelIndex index = model.index( row, column );
+            model.setData( index, QVariant( i++  ) );
+        }
+    }
+
+    // we want a polar diagram on a matching coordinate plane
+    PolarDiagram diag;
+    PolarCoordinatePlane plane( &chart );
+    diag.setModel( &model );
+
+    chart.replaceCoordinatePlane( &plane );
+    chart.coordinatePlane()->replaceDiagram( &diag );
+
+    chart.show();
+
+    return app.exec();
+}
