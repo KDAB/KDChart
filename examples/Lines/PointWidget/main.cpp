48489e0ce8515bf71ab2d13e5d763c6995c8eb51
Michel Boyer de la Giroday <michel@kdab.com>
2006-11-23 15:28:22 +0000

    improve display
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@3658 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/Lines/PointWidget/main.cpp
+++ examples/Lines/PointWidget/main.cpp
@@ -61,6 +61,7 @@ int main( int argc, char** argv ) {
     dva.setVisible( true );
 
     widget.lineDiagram()->setDataValueAttributes( dva );
+    widget.setGlobalLeading( 10,  10,  10, 10 );
 
     widget.show();
 
