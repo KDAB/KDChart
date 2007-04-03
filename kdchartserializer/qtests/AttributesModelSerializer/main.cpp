f4cd2f923e7f2022cc3520a4a7f05bc0695534cb
Karl-Heinz Zimmer <khz@klaralvdalens-datakonsult.se>
2007-04-03 18:49:33 +0000

    parse the AttributesModel (but there is still one issue left in this, so qtests will go wrong)
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@7408 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ kdchartserializer/qtests/AttributesModelSerializer/main.cpp
@@ -0,0 +1,166 @@
+#include <QtTest/QtTest>
+#include <TableModel.h>
+#include <KDChartGlobal>
+
+#include <KDChartChart>
+
+#include <KDChartBarDiagram>
+#include <KDChartLineDiagram>
+#include <KDChartPieDiagram>
+
+#include <KDChartCartesianCoordinatePlane>
+#include <KDChartDataValueAttributes>
+
+#include <KDChartSerializeCollector>
+#include <KDChartAttributesSerializer>
+#include <KDChartAttributesModelSerializer>
+#include <KDXMLTools>
+
+#include <iostream>
+
+using namespace KDChart;
+
+class TestKDChartAttributesModelSerializer : public QObject {
+  Q_OBJECT
+private slots:
+
+    void initTestCase()
+    {
+        mDocstart = "<kdchart:kdchart/>";
+        resetDoc();
+
+        m_chart = new Chart(0);
+        TableModel *tableModel = new TableModel( this );
+        tableModel->loadFromCSV( "../../../examples/tools/modeldata/KDChart-Test-Datatables.csv" );
+        tableModel->setSupplyHeaderData( false );
+        m_model = tableModel;
+
+        m_lines = new LineDiagram();
+        m_lines->setModel( m_model );
+
+        DataValueAttributes da( m_lines->dataValueAttributes( 2 ) );
+        RelativePosition negPos( da.negativePosition() );
+        negPos.setReferenceArea( 0 );
+        RelativePosition posPos( da.positivePosition() );
+        posPos.setReferenceArea( 0 );
+        TextAttributes ta( da.textAttributes() );
+        Measure me( ta.fontSize() );
+        me.setReferenceArea( 0 );
+        ta.setFontSize( me );
+        ta.setMinimalFontSize( me );
+        MarkerAttributes ma( da.markerAttributes() );
+        ma.setMarkerStyle( MarkerAttributes::MarkerRing );
+
+        da.setNegativePosition( negPos );
+        da.setPositivePosition( posPos );
+        da.setTextAttributes( ta );
+        da.setMarkerAttributes( ma );
+        m_lines->setDataValueAttributes( 2, da );
+
+        QModelIndex idx = m_model->index( 0, 2, QModelIndex() );
+        da = m_lines->dataValueAttributes( idx );
+        ma.setMarkerStyle( MarkerAttributes::MarkerFastCross );
+
+        da.setNegativePosition( negPos );
+        da.setPositivePosition( posPos );
+        da.setTextAttributes( ta );
+        da.setMarkerAttributes( ma );
+        m_lines->setDataValueAttributes( idx, da );
+
+
+        m_attrsModel = m_lines->attributesModel();
+        mAttrModelS = new AttributesModelSerializer();
+    }
+
+    void testAttributesModel()
+    {
+        resetDoc();
+
+        QDomElement savedElement =
+                mDoc.createElement( "TESTING" );
+        mDocRoot.appendChild( savedElement );
+
+        AttributesModel orgAttrsModel(0,0);
+
+        orgAttrsModel.initFrom( m_attrsModel );
+
+        mAttrModelS->saveAttributesModel(
+                mDoc,
+                savedElement,
+                &orgAttrsModel );
+        SerializeCollector::instance()->appendDataToElement(
+                mDoc, mDocRoot, "kdchart:global-objects" );
+
+        // use cout rather that qDebug() to avoid the length limitation of the later
+        std::cout << "\n\n" << mDoc.toString(2).toLatin1().data() << "\n\n";
+
+        bool bFoundSavedAttributesModel = false;
+        QDomElement parsedElement;
+        QDomNode node = mDocRoot.firstChild();
+        while( !node.isNull() ) {
+            QDomElement element = node.toElement();
+            if( !element.isNull() ) { // was really an element
+                QString tagName = element.tagName();
+                if( tagName == "kdchart:global-objects" ) {
+                    QDomNode node2 = element.firstChild();
+                    while( !node2.isNull() ) {
+                        QDomElement ele2 = node2.toElement();
+                        if( !ele2.isNull() ) { // was really an element
+                            QString tagName2 = ele2.tagName();
+                            if( tagName2 == "kdchart:attribute-models" ) {
+                                QDomNode node3 = ele2.firstChild();
+                                while( !node3.isNull() ) {
+                                    QDomElement ele3 = node3.toElement();
+                                    if( !ele3.isNull() ) { // was really an element
+                                        QString tagName3 = ele3.tagName();
+                                        if( tagName3 == "kdchart:attribute-model:1" ) {
+                                            parsedElement = ele3;
+                                            bFoundSavedAttributesModel = true;
+                                        }
+                                    }
+                                    node3 = node3.nextSibling();
+                                }
+                            }
+                        }
+                        node2 = node2.nextSibling();
+                    }
+                }
+            }
+            node = node.nextSibling();
+        }
+        QVERIFY( bFoundSavedAttributesModel );
+
+        AttributesModel parsedAttrsModel(0, 0);
+        QVERIFY( mAttrModelS->parseAttributesModel( parsedElement, parsedAttrsModel ) );
+        QVERIFY( orgAttrsModel.hasEqualSettings( &parsedAttrsModel ) );
+    }
+
+
+    void cleanupTestCase()
+    {
+        delete m_chart;
+    }
+
+private:
+    void resetDoc()
+    {
+        mDoc = QDomDocument( "KDChart" );
+        mDoc.setContent( mDocstart );
+        mDocRoot = mDoc.documentElement();
+    }
+
+    QString mDocstart;
+    QDomDocument mDoc;
+    QDomElement mDocRoot;
+
+    Chart *m_chart;
+    QAbstractItemModel *m_model;
+    AttributesModel *m_attrsModel;
+    LineDiagram *m_lines;
+
+    AttributesModelSerializer *mAttrModelS;
+};
+
+QTEST_MAIN(TestKDChartAttributesModelSerializer)
+
+#include "main.moc"
