05324719f65222b5ebee65178e3e9a54c7b5bab3
Mirko Boehm <mirko.boehm@kdab.com>
2007-07-03 12:42:56 +0000

    r4788@cutiepie:  mirkoboehm | 2007-07-03 13:37:14 +0100  - add unit test for QVariant brute-force serialization
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@9384 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ qtests/XmlToolsTests/main.cpp
@@ -0,0 +1,67 @@
+#include <KDXMLTools>
+
+#include <QtTest/QtTest>
+#include <QVariant>
+
+class XmlToolsTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+
+    void initTestCase()
+    {
+    }
+
+    void testBool()
+    {
+        runTests( QVariantList() << QVariant( true ) << QVariant( false ) );
+    }
+
+    void testQString()
+    {
+        runTests( QVariantList()
+                  << QVariant( QString() ) // an empty one
+                  << QVariant( QString( "some text" ) ) );
+    }
+
+    void cleanupTestCase()
+    {
+    }
+
+private:
+    void runTests( const QVariantList& variants )
+    {
+        static const QString name( "varianttests" );
+        QDomDocument doc( "test" );
+        QDomElement root = doc.createElement( "properties" );
+        doc.appendChild( root );
+
+        Q_FOREACH( QVariant value, variants ) {
+            // serialize value, deserialize it, and compare the results
+            KDXML::createQVariantNode( doc, root, name, value );
+        }
+
+        QDomNodeList elements = root.childNodes();
+        QCOMPARE( variants.size(), elements.size() );
+
+        int at = 0;
+        Q_FOREACH( QVariant value, variants ) {
+            QDomElement element = elements.at( at ).toElement();
+            ++at;
+            QVariant result;
+            QString nameResult;
+            bool ok = KDXML::readQVariantNode( element, result, nameResult );
+            QVERIFY2( ok, "deserializing failed" );
+            QCOMPARE( nameResult, name );
+            QCOMPARE( value, result );
+        }
+    }
+
+    QVariantList mTestcases;
+    QString mDocstart;
+};
+
+QTEST_MAIN(XmlToolsTest)
+
+#include "main.moc"
