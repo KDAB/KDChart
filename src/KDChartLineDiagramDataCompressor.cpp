4cadb4e123c1cad1f5713be59d18c787a7c9aa63
Mirko Boehm <mirko.boehm@kdab.com>
2007-07-06 08:31:49 +0000

    r5022@cutiepie:  mirkoboehm | 2007-07-05 12:23:20 +0100  - start implementing line diagram model data compression  - this is somewhat of a work-in-progress, thus it is so far completely hidden in private API  - the approximation mode setting + enum may have to become public
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@9450 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ src/KDChartLineDiagramDataCompressor.cpp
@@ -0,0 +1,1 @@
+#include "KDChartLineDiagramDataCompressor_p.h"
---
+++ src/KDChartLineDiagramDataCompressor.h
@@ -0,0 +1,58 @@
+#ifndef KDCHARTLINEDIAGRAMDATACOMPRESSOR_H
+#define KDCHARTLINEDIAGRAMDATACOMPRESSOR_H
+
+#include <QObject>
+#include <QModelIndex>
+
+namespace KDChart {
+
+    // - transparently compress table model data if the diagram widget
+    // size does not allow to display all data points in an acceptable way
+    // - the class acts much like a proxy model, but is not
+    // implemented as one, to avoid performance penalties by QVariant
+    // conversions
+    // - a wanted side effect is that the compressor will deliver
+    // more precise values for more precise media, like paper
+    // (a) this is absolutely strictly seriously private API of KDChart
+    // (b) if possible, this class is going to be templatized for
+    // different diagram types
+    class LineDiagramDataCompressor : public QObject
+    {
+        Q_OBJECT
+
+    public:
+        class DataPoint {
+            double value;
+            QModelIndex index;
+        };
+        typedef QVector<DataPoint> DataPointVector;
+
+        enum ApproximationMode {
+            // do not approximate
+            Precise,
+            // approximate by averaging out over prime number distances
+            SamplingSeven
+        };
+
+        explicit LineDiagramDataCompressor( QObject* parent );
+
+        // input: model, chart resolution, approximation mode
+        void setModel( AttributesModel* );
+        void setResolution( int x, int y );
+        void setApproximationMode( ApproximationMode mode );
+
+        // output: resulting model resolution, data points
+        int modelDataColumns() const;
+        int modelDataRows() const;
+        const DataPoint& data( int row, int column ) const;
+
+    public slots:
+        void slotModelDataChanged();
+
+    private:
+        // one per dataset
+        QVector<DataPointVector> m_data;
+    };
+}
+
+#endif
