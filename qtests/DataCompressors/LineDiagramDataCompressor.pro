591b00a09104be58633300167ed737cd09d2915f
Mirko Boehm <mirko.boehm@kdab.com>
2007-07-06 08:32:05 +0000

    r5025@cutiepie:  mirkoboehm | 2007-07-05 12:59:42 +0100  - add unit tests for data compressors
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@9453 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ qtests/DataCompressors/LineDiagramDataCompressor.pro
@@ -0,0 +1,6 @@
+KDCHART_PATH = ../..
+include( ../qtests.pri )
+
+SOURCES += LineDiagramDataCompressorTests.cpp
+
+message( "Building ''$$TARGET'' using LIBS ''$$LIBS''" )
---
+++ qtests/DataCompressors/LineDiagramDataCompressorTests.cpp
@@ -0,0 +1,21 @@
+#include <QtTest/QtTest>
+
+class LineDiagramDataCompressorTests : public QObject
+{
+    Q_OBJECT
+
+private slots:
+
+    void initTestCase()
+    {
+    }
+
+    void cleanupTestCase()
+    {
+    }
+
+};
+
+QTEST_MAIN(LineDiagramDataCompressorTests)
+
+#include "LineDiagramDataCompressorTests.moc"
