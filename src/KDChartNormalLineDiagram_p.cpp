ef1e4588272c69ef190bf5da8049f0fbff1d4200
Mirko Boehm <mirko.boehm@kdab.com>
2007-07-10 20:27:17 +0000

    r5104@cutiepie:  mirkoboehm | 2007-07-10 22:09:02 +0200  - down with the FIXMEs (this time: "Do not use well-known class names like list as variable names)  - remove commented out code
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/trunk/kdchart@9532 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/KDChartNormalLineDiagram_p.cpp
+++ src/KDChartNormalLineDiagram_p.cpp
@@ -56,21 +56,13 @@ void NormalLineDiagram::paint(  PaintContext* ctx )
 {
     reverseMapper().clear();
     // FIXME remove hungarian notation: iColumn, bOK, bValuesFound
-    // FIXME do not use STL class names for variable names, like "list"
-    // remove, we are not a bar diagram:
-//     const bool shiftCountedXValuesByHalfSection =
-//         (dynamic_cast< BarDiagram* >( diagram()->referenceDiagram() ) != 0);
-
-//     const QPair<QPointF, QPointF> boundaries = diagram()->dataBoundaries(); // cached
-//     const QPointF& bottomLeft = boundaries.first;
-//     const QPointF& topRight = boundaries.second;
     Q_ASSERT( dynamic_cast<CartesianCoordinatePlane*>( ctx->coordinatePlane() ) );
     CartesianCoordinatePlane* plane = static_cast<CartesianCoordinatePlane*>( ctx->coordinatePlane() );
     const int columnCount = compressor().modelDataColumns();
     const int rowCount = compressor().modelDataRows();
     if ( columnCount == 0 || rowCount == 0 ) return; // maybe blank out the area?
 
-    // FIXME integrate column index retrieval to compressor:
+// FIXME integrate column index retrieval to compressor:
     int maxFound = 0;
 //     {   // find the last column number that is not hidden
 //         for( int iColumn =  datasetDimension() - 1;
@@ -81,10 +73,8 @@ void NormalLineDiagram::paint(  PaintContext* ctx )
 //     }
     maxFound = columnCount;
     // ^^^ temp
-    const int lastVisibleColumn = maxFound;
-    // const int rowCount = attributesModel()->rowCount( attributesModelRootIndex() );
 
-    DataValueTextInfoList list;
+    DataValueTextInfoList textInfoList;
     LineAttributesInfoList lineList;
     LineAttributes::MissingValuesPolicy policy; // ???
 
@@ -109,7 +99,7 @@ void NormalLineDiagram::paint(  PaintContext* ctx )
                 lineList.append( LineAttributesInfo( point.index, a, b ) );
                 // add data point labels:
                 const PositionPoints pts = point.value > 0 ? PositionPoints( b, a, d, c ) : PositionPoints( d, c, b, a );
-                appendDataValueTextInfoToList( diagram(), list, point.index, pts,
+                appendDataValueTextInfoToList( diagram(), textInfoList, point.index, pts,
                                                Position::NorthWest, Position::SouthWest,
                                                point.value );
                 // if necessary, add the area to the area list:
@@ -127,146 +117,7 @@ void NormalLineDiagram::paint(  PaintContext* ctx )
         }
     }
 
-    paintElements( ctx, list, lineList, policy );
-
-/*
-    bool bValuesFound = false;
-    double lastValueX, lastValueY;
-    double valueX, valueY;
-    for ( int iRow = 0; iRow < rowCount; ++iRow ) {
-        bool skipThisCell = false;
-        // trying to find a fromPoint
-        // FIXME this needs to use an index
-
-        // all that policy stuff will be completely hidden in the compressor:
-        // policy = getCellValues( iRow, iColumn,
-//                                     shiftCountedXValuesByHalfSection,
-//                                     valueX, valueY );
-        switch( policy ){
-        case LineAttributes::MissingValuesAreBridged:
-            if( bValuesFound ){
-                valueX = lastValueX;
-                valueY = lastValueY;
-            }else{
-                skipThisCell = true;
-            }
-            break;
-        case LineAttributes::MissingValuesHideSegments:
-            skipThisCell = true;
-            break;
-        case LineAttributes::MissingValuesShownAsZero:
-// fall through intended
-        case LineAttributes::MissingValuesPolicyIgnored:
-            lastValueX = valueX;
-            lastValueY = valueY;
-            bValuesFound = true;
-            break;
-        }
-        if( ! skipThisCell ){
-// trying to find a toPoint
-            double nextValueX, nextValueY;
-            bool foundToPoint = false;
-            int iNextRow = iRow+1;
-            while ( ! (foundToPoint || skipThisCell || iNextRow >= rowCount) ) {
-                policy = getCellValues(
-                    iNextRow, iColumn,
-                    shiftCountedXValuesByHalfSection,
-                    nextValueX, nextValueY );
-                switch( policy ){
-                case LineAttributes::MissingValuesAreBridged:
-// The cell has no valid value, so we  make sure that
-// this cell will not be processed by the next iteration
-// of the iRow loop:
-                    ++iRow;
-                    break;
-                case LineAttributes::MissingValuesHideSegments:
-// The cell has no valid value, so we  make sure that
-// this cell will not be processed by the next iteration
-// of the iRow loop:
-                    skipThisCell = true;
-                    ++iRow;
-                    break;
-                case LineAttributes::MissingValuesShownAsZero:
-// fall through intended
-                case LineAttributes::MissingValuesPolicyIgnored:
-                    foundToPoint = true;
-                    break;
-                }
-                ++iNextRow;
-            }
-            if( ! skipThisCell ){
-                const bool isPositive = (valueY >= 0.0);
-                const QModelIndex index = diagram()->model()->index( iRow, iColumn, diagram()->rootIndex() );
-                const LineAttributes laCell = diagram()->lineAttributes( index );
-                const bool bDisplayCellArea = laCell.displayArea();
-
-                QPointF fromPoint = ctx->coordinatePlane()->translate( QPointF( valueX, valueY ) );
-
-                const QPointF ptNorthWest(
-                    (bDisplayCellArea && ! isPositive)
-                    ? ctx->coordinatePlane()->translate( QPointF( valueX, 0.0 ) )
-                    : fromPoint );
-                const QPointF ptSouthWest(
-                    (bDisplayCellArea && isPositive)
-                    ? ctx->coordinatePlane()->translate( QPointF( valueX, 0.0 ) )
-                    : fromPoint );
-//qDebug() << "--> ptNorthWest:" << ptNorthWest;
-//qDebug() << "--> ptSouthWest:" << ptSouthWest;
-                QPointF ptNorthEast;
-                QPointF ptSouthEast;
-
-                if( foundToPoint ){
-                    QPointF toPoint = ctx->coordinatePlane()->translate( QPointF( nextValueX, nextValueY ) );
-                    lineList.append( LineAttributesInfo( index, fromPoint, toPoint ) );
-                    ptNorthEast =
-                        (bDisplayCellArea && ! isPositive)
-                        ? ctx->coordinatePlane()->translate( QPointF( nextValueX, 0.0 ) )
-                        : toPoint;
-                    ptSouthEast =
-                        (bDisplayCellArea && isPositive)
-                        ? ctx->coordinatePlane()->translate( QPointF( nextValueX, 0.0 ) )
-                        : toPoint;
-// we can't take as a condition the line attributes
-// to be different from a cell to another.
-// e.g the user should be able to have different brush
-// which is not reflected in the line attributes
-// see examples/Area which show such an option
-                    if( areas.count() ) { // && laCell != laPreviousCell )
-                        paintAreas( ctx, indexPreviousCell, areas, laPreviousCell.transparency() );
-                        areas.clear();
-                    }
-                    if( bDisplayCellArea ){
-                        QPolygonF poly;
-                        poly << ptNorthWest << ptNorthEast << ptSouthEast << ptSouthWest;
-                        //qDebug() << "ptNorthWest:" << ptNorthWest;
-                        //qDebug() << "ptNorthEast:" << ptNorthEast;
-                        //qDebug() << "ptSouthEast:" << ptSouthEast;
-                        //qDebug() << "ptSouthWest:" << ptSouthWest;
-                        //qDebug() << "polygon:" << poly;
-                        areas << poly;
-                        laPreviousCell = laCell;
-                        indexPreviousCell = index;
-                    }
-                }else{
-                    ptNorthEast = ptNorthWest;
-                    ptSouthEast = ptSouthWest;
-                }
-
-                const PositionPoints pts = isPositive ? PositionPoints( ptNorthWest, ptNorthEast, ptSouthEast, ptSouthWest )
-                                           : PositionPoints( ptSouthWest, ptSouthEast, ptNorthEast, ptNorthWest );
-                appendDataValueTextInfoToList( diagram(), list, index, pts,
-                                               Position::NorthWest, Position::SouthWest,
-                                               valueY );
-            }
-        }
-    }
-    if( areas.count() ){
-        paintAreas( ctx, indexPreviousCell, areas, laPreviousCell.transparency() );
-        areas.clear();
-    }
-}
-    paintElements( ctx, list, lineList, policy );
-*/
+    paintElements( ctx, textInfoList, lineList, policy );
 }
 
 
