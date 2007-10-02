e21985319dcc080ded56ec4ffc6364ecbc9bea2d
Tobias Koenig <tobias.koenig@kdab.com>
2007-10-02 09:43:15 +0000

    Fixed #4248 -  (Vista) Polar diagram bug: moving the long slider back and forth The points ended up outside the grid when the absolute value of a negative value was greater than the greatest positive value.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@10913 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/KDChartPolarDiagram.cpp
+++ src/KDChartPolarDiagram.cpp
@@ -108,7 +108,7 @@ const QPair<QPointF, QPointF> PolarDiagram::calculateDataBoundaries () const
     for ( int j=0; j<colCount; ++j ) {
         for ( int i=0; i< rowCount; ++i ) {
             double value = model()->data( model()->index( i, j, rootIndex() ) ).toDouble();
-            yMax = qMax( yMax, value );
+            yMax = qMax( yMax, qAbs(value) );
         }
     }
     QPointF bottomLeft ( QPointF( xMin, yMin ) );
