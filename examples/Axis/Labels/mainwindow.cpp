a2904815fb7bdbbb3b8890488f9b391f568a3c97
Karl-Heinz Zimmer <khz@klaralvdalens-datakonsult.se>
2009-01-06 19:47:15 +0000

    show axis labels rotated by 69 degrees (see issue #7606)
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@23426 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/Axis/Labels/mainwindow.cpp
+++ examples/Axis/Labels/mainwindow.cpp
@@ -29,8 +29,6 @@
 #include <KDChartAbstractCoordinatePlane>
 #include <KDChartBackgroundAttributes>
 #include <KDChartChart>
-#include <KDChartDataValueAttributes>
-#include <KDChartHeaderFooter>
 #include <KDChartLegend>
 #include <KDChartLineDiagram>
 #include <KDChartLineAttributes>
@@ -65,40 +63,8 @@ MainWindow::MainWindow( QWidget* parent ) :
     yAxis->setBounds(3, 6);
     xAxis->setPosition ( KDChart::CartesianAxis::Bottom );
     yAxis->setPosition ( KDChart::CartesianAxis::Left );
-    xAxis->setTitleText ( "a default Abscissa axis at the bottom" );
-    yAxis->setTitleText ( "a custom Ordinate axis at the left side" );
 
-    // add a header
-    KDChart::HeaderFooter* headerFooter = new KDChart::HeaderFooter( m_chart );
-    headerFooter->setType( KDChart::HeaderFooter::Header );
-    headerFooter->setPosition( KDChart::Position::North );
-    headerFooter->setText( "Line diagram using a custom axis class" );
-    m_chart->addHeaderFooter( headerFooter );
-    // adjust the text colour of the header
-    KDChart::TextAttributes textAttrs( headerFooter->textAttributes() );
-    textAttrs.setPen( QPen( Qt::red ) );
-    headerFooter->setTextAttributes( textAttrs );
-
-    // add a footer
-    headerFooter = new KDChart::HeaderFooter( m_chart );
-    headerFooter->setType( KDChart::HeaderFooter::Footer );
-    headerFooter->setPosition( KDChart::Position::West );
-    headerFooter->setText( "see also kdchartserializer/examples/customAxisClass/" );
-    m_chart->addHeaderFooter( headerFooter );
-    // adjust the text attributes: declare a tiny fixed font size and white color
-    textAttrs = headerFooter->textAttributes();
-    KDChart::Measure me( 7.0, KDChartEnums::MeasureCalculationModeAbsolute );
-    textAttrs.setFontSize( me );
-    textAttrs.setMinimalFontSize( me );
-    textAttrs.setPen( QPen( Qt::white ) );
-    headerFooter->setTextAttributes( textAttrs );
-    // adjust ther background colour
-    KDChart::BackgroundAttributes ba = headerFooter->backgroundAttributes();
-    ba.setVisible( true );
-    ba.setBrush( QBrush( QColor(0x60,0x60,0x60) ) );
-    headerFooter->setBackgroundAttributes( ba );
-
-
+/*
 // set the following to 0, to have only one of the axes with background
 #if 1
     // colourize the axes' backgrounds
@@ -117,7 +83,7 @@ MainWindow::MainWindow( QWidget* parent ) :
     ba.setBrush( linearGrad );
     yAxis->setBackgroundAttributes( ba );
 #endif
-
+*/
     // add 1 pixel space at the left and at the top edge, because the
     // axis area would otherwise overwrite the left/top borders
     m_chart->setGlobalLeading(1,1,0,0);
@@ -129,8 +95,14 @@ MainWindow::MainWindow( QWidget* parent ) :
     xAxis->setLabels( daysOfWeek );
     QStringList shortDays;
     shortDays << "Mon" << "Tue" << "Wed" << "Thu" << "Fri" << "Sat" << "Sun";
-    xAxis->setShortLabels( shortDays );
+    //xAxis->setShortLabels( shortDays );
 #endif
+
+
+    // rotate abscissa labels by -60 degrees:
+    KDChart::TextAttributes ta( xAxis->textAttributes() );
+    ta.setRotation( -60 );
+    xAxis->setTextAttributes( ta );
 
     m_lines->addAxis( xAxis );
     m_lines->addAxis( yAxis );
@@ -138,6 +110,8 @@ MainWindow::MainWindow( QWidget* parent ) :
     m_chart->coordinatePlane()->replaceDiagram( m_lines );
     // Set up the legend
     m_legend = new KDChart::Legend( m_lines, m_chart );
+    m_legend->setPosition( KDChart::Position::East );
+    m_legend->setAlignment( Qt::AlignTop );
     m_chart->addLegend( m_legend );
     m_legend->hide();
 }
