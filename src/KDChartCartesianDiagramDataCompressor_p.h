eb19f27585cf4497e6be317136ed9e53f7765e7a
Sebastian Sauer <sebastian.sauer@kdab.com>
2010-01-14 16:24:48 +0000

    s/double/qreal/
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@32523 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/KDChartCartesianDiagramDataCompressor_p.h
+++ src/KDChartCartesianDiagramDataCompressor_p.h
@@ -72,12 +72,12 @@ namespace KDChart {
         class DataPoint {
         public:
             DataPoint() 
-                : key( std::numeric_limits< double >::quiet_NaN() ), 
-                  value( std::numeric_limits< double >::quiet_NaN() ), 
+                : key( std::numeric_limits< qreal >::quiet_NaN() ), 
+                  value( std::numeric_limits< qreal >::quiet_NaN() ), 
                   hidden( false ) 
                   {}
-            double key;
-            double value;
+            qreal key;
+            qreal value;
             bool hidden;
             QModelIndex index;
         };
@@ -194,7 +194,7 @@ namespace KDChart {
         QPointer<QAbstractItemModel> m_model;
         unsigned int m_sampleStep;
         QModelIndex m_rootIndex;
-        ModelDataCache< double > m_modelCache;
+        ModelDataCache< qreal > m_modelCache;
         mutable DataValueAttributesCache m_dataValueAttributesCache;
         int m_datasetDimension;
     };
