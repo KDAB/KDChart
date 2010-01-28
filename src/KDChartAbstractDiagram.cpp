KDAB/KDChart

commit 65e03286f19ae694c48d35f7e6f8bca16b4130fc

apply patch by Prashanth KDCH-510: changing the data in the model does not automatically change the graphic
git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@32889 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

src/KDChartAbstractDiagram.cpp
@@ -263,6 +263,7 @@ void AbstractDiagram::setModel( QAbstractItemModel * newModel )
         connect( model(), SIGNAL( columnsRemoved( QModelIndex, int, int ) ), this, SLOT( setDataBoundariesDirty() ) );
         connect( model(), SIGNAL( modelReset() ), this, SLOT( setDataBoundariesDirty() ) );
         connect( model(), SIGNAL( layoutChanged() ), this, SLOT( setDataBoundariesDirty() ) );
+        connect( model(), SIGNAL( dataChanged(QModelIndex,QModelIndex) ), this, SIGNAL( modelDataChanged() ));
     }
     emit modelsChanged();
 }

src/KDChartAbstractCoordinatePlane.cpp
@@ -83,6 +83,8 @@ void AbstractCoordinatePlane::addDiagram ( AbstractDiagram* diagram )
     layoutDiagrams();
     layoutPlanes(); // there might be new axes, etc
     connect( diagram, SIGNAL( modelsChanged() ), this, SLOT( layoutPlanes() ) );
+    connect( diagram, SIGNAL( modelDataChanged() ), this, SLOT( update()) );
+
     update();
 }
 
@@ -116,6 +118,7 @@ void AbstractCoordinatePlane::takeDiagram ( AbstractDiagram* diagram )
         diagram->setParent( 0 );
         diagram->setCoordinatePlane( 0 );
         disconnect( diagram, SIGNAL( modelsChanged() ), this, SLOT( layoutPlanes() ) );
+        disconnect( diagram, SIGNAL( modelDataChanged() ), this, SLOT( update()) );
         layoutDiagrams();
         update();
     }

src/KDChartAbstractDiagram.h
@@ -673,6 +673,9 @@ namespace KDChart {
         /** This signal is emitted, when either the model or the AttributesModel is replaced. */
         void modelsChanged();
 
+        /** This signal is emitted, when the model data is changed. */
+        void modelDataChanged();
+
         /** This signal is emitted, when the hidden status of at least one data cell was (un)set. */
         void dataHidden();
 

commit b2d22fce35a10f0ff24dc7068f668b6a88bfb185

Fix bug introduced in r32889; disconnect all signals from the old model even those newly introduced to fix KDCH-510
git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@32892 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

src/KDChartAbstractDiagram.cpp
@@ -248,6 +248,7 @@ void AbstractDiagram::setModel( QAbstractItemModel * newModel )
         disconnect( model(), SIGNAL( columnsRemoved( QModelIndex, int, int ) ), this, SLOT( setDataBoundariesDirty() ) );
         disconnect( model(), SIGNAL( modelReset() ), this, SLOT( setDataBoundariesDirty() ) );
         disconnect( model(), SIGNAL( layoutChanged() ), this, SLOT( setDataBoundariesDirty() ) );
+        disconnect( model(), SIGNAL( dataChanged(QModelIndex,QModelIndex) ), this, SIGNAL( modelDataChanged() ));
     }
     QAbstractItemView::setModel( newModel );
     AttributesModel* amodel = new PrivateAttributesModel( newModel, this );