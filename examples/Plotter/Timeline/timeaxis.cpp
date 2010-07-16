584e77343e6f88ba2698504356a0bfc2d7f2f5ee
Sebastian Sauer <sebastian.sauer@kdab.com>
2010-07-16 14:06:22 +0000

    extend the sample with some more example code (the scientific notation case was a customers question)
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@36052 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/Plotter/Timeline/timeaxis.cpp
+++ examples/Plotter/Timeline/timeaxis.cpp
@@ -10,6 +10,13 @@ TimeAxis::TimeAxis( KDChart::AbstractCartesianDiagram* parent )
 
 const QString TimeAxis::customizedLabel( const QString& label ) const
 {
+    // Here we are free to format the value to whatever we want. As example we
+    // could also return a scientific notation with something like the following
+    // both lines;
+    //const int precision = 2;
+    //return QString::number(label.toDouble(), 'E', precision);
+
+    // Format the time-value to a nice string representation.
     const QDateTime dateTime = QDateTime::fromTime_t( label.toDouble() * 3600.0 );
     return dateTime.date().toString();
 }
