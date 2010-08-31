193310b47a4b830687187a1ed8b0b56af2717a2c
Sebastian Sauer <sebastian.sauer@kdab.com>
2010-08-31 12:50:42 +0000

    Extend the timeline-sample with some Realtime functionality to demonstrate how both can be combined. This fixed KDCH-707.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@36744 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/Plotter/Timeline/main.cpp
+++ examples/Plotter/Timeline/main.cpp
@@ -1,6 +1,10 @@
 #include <QApplication>
 #include <QStandardItemModel>
 #include <QTreeView>
+#include <QTimer>
+#include <QWidget>
+#include <QSplitter>
+#include <QPushButton>
 
 #include <math.h>
 
@@ -10,6 +14,82 @@
 #include "timeaxis.h"
 #include "timechartmodel.h"
 
+class ChartWidget : public QWidget {
+    Q_OBJECT
+public:
+    explicit ChartWidget(QWidget* parent = 0) : QWidget(parent), m_counter(0) {
+        QSplitter* splitter = new QSplitter(this);
+        QHBoxLayout* l = new QHBoxLayout(this);
+        setLayout(l);
+        l->addWidget( splitter );
+
+        QWidget* leftWidget = new QWidget( splitter );
+        QVBoxLayout* leftLayout = new QVBoxLayout(leftWidget);
+        leftWidget->setLayout(leftLayout);
+        
+        QPushButton* button = new QPushButton("Animate", leftWidget);
+        leftLayout->addWidget( button );
+        button->setCheckable( true );
+        connect( button, SIGNAL(toggled(bool)), this, SLOT(buttonToggled(bool)) );
+
+        QTreeView* tv = new QTreeView( leftWidget );
+        leftLayout->addWidget( tv );
+
+        m_chart = new KDChart::Chart( splitter );
+        
+        m_model = new QStandardItemModel( 365, 2, this );
+        for( int i = 0; i < 365; ++i ) {
+            const QDateTime dt = QDateTime( QDate( 2010, 1, 1 ), QTime() );
+            m_model->setData( m_model->index( i, 0 ), dt.addDays( i ) );
+            m_model->setData( m_model->index( i, 1 ), sin( i / 10.0 ) * 10 );
+        }
+
+        TimeChartModel* proxy = new TimeChartModel( this );
+        proxy->setSourceModel( m_model );
+        proxy->setVisibleRange( QDateTime( QDate( 2010, 2, 1 ), QTime() ), QDateTime( QDate( 2010, 3, 31 ), QTime() ) );
+
+        KDChart::Plotter* plotter = new KDChart::Plotter;
+        m_chart->coordinatePlane()->replaceDiagram( plotter );
+
+        tv->setModel( proxy );
+        tv->show();
+
+        TimeAxis* axis = new TimeAxis( plotter );
+        axis->setPosition( TimeAxis::Bottom );
+        plotter->addAxis( axis );
+
+        plotter->setModel( proxy );
+
+        QObject::connect( proxy, SIGNAL( rowsInserted( QModelIndex, int, int ) ), m_chart->coordinatePlane(), SLOT( adjustRangesToData() ), Qt::QueuedConnection );
+        QObject::connect( proxy, SIGNAL( rowsRemoved( QModelIndex, int, int ) ), m_chart->coordinatePlane(), SLOT( adjustRangesToData() ), Qt::QueuedConnection );
+        
+        dynamic_cast< KDChart::CartesianCoordinatePlane* >( m_chart->coordinatePlane() )->setHorizontalRange( qMakePair< qreal, qreal >( 1264978800, 1269986400 ) );
+        proxy->setVisibleRange( QDateTime( QDate( 2010, 3, 15 ), QTime() ), QDateTime( QDate( 2010, 5, 18 ), QTime() ) );
+
+        m_timer = new QTimer(this);
+        connect( m_timer, SIGNAL( timeout() ), this, SLOT( slotTimeout() ) );
+    }
+private slots:
+    void slotTimeout() {
+        QVariant v1, v2;
+        for( int i = 0; i < 365; ++i ) {
+            m_model->setData( m_model->index( i, 1 ), sin( i / 10.0+m_counter ) * 10 );
+        }
+        m_counter += 0.02;
+    }
+    void buttonToggled(bool checked) {
+        if(checked)
+            m_timer->start( 200 );
+        else
+            m_timer->stop();
+    }
+private:
+    KDChart::Chart* m_chart;
+    QStandardItemModel* m_model;
+    QTimer* m_timer;
+    qreal m_counter;
+};
+
 /**
  * This example demonstrates how to use time-based plots with timestamp-value data points
  * based on seconds and how to use a proxy model for defining the plotted "window" of the
@@ -18,42 +98,11 @@
 int main( int argc, char* argv[] )
 {
     QApplication app( argc, argv );
-
-    QStandardItemModel model( 365, 2 );
-    for( int i = 0; i < 365; ++i )
-    {
-        const QDateTime dt = QDateTime( QDate( 2010, 1, 1 ), QTime() );
-        model.setData( model.index( i, 0 ), dt.addDays( i ) );
-        model.setData( model.index( i, 1 ), sin( i / 10.0 ) * 10 );
-    }
-
-    TimeChartModel proxy;
-    proxy.setSourceModel( &model );
-    proxy.setVisibleRange( QDateTime( QDate( 2010, 2, 1 ), QTime() ), 
-                           QDateTime( QDate( 2010, 3, 31 ), QTime() ) );
-
-    KDChart::Chart* chart = new KDChart::Chart;
-    KDChart::Plotter* plotter = new KDChart::Plotter;
-    chart->coordinatePlane()->replaceDiagram( plotter );
-
-    QTreeView tv;
-    tv.setModel( &proxy );
-    tv.show();
-
-    TimeAxis* axis = new TimeAxis( plotter );
-    axis->setPosition( TimeAxis::Bottom );
-    plotter->addAxis( axis );
-
-    plotter->setModel( &proxy );
-
-    QObject::connect( &proxy, SIGNAL( rowsInserted( QModelIndex, int, int ) ), chart->coordinatePlane(), SLOT( adjustRangesToData() ), Qt::QueuedConnection );
-    QObject::connect( &proxy, SIGNAL( rowsRemoved( QModelIndex, int, int ) ), chart->coordinatePlane(), SLOT( adjustRangesToData() ), Qt::QueuedConnection );
     
-    dynamic_cast< KDChart::CartesianCoordinatePlane* >( chart->coordinatePlane() )->setHorizontalRange( qMakePair< qreal, qreal >( 1264978800, 1269986400 ) );
-    proxy.setVisibleRange( QDateTime( QDate( 2010, 3, 15 ), QTime() ), 
-                           QDateTime( QDate( 2010, 5, 18 ), QTime() ) );
-
-    chart->show();
+    ChartWidget w;
+    w.show();
 
     return app.exec();
 }
+
+#include "main.moc"
