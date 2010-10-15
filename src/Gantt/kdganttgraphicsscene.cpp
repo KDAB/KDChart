8b1963550a8b6eaa62271feb98b1f9705518361b
Steffen Hansen <steffen.hansen@kdab.com>
2010-10-15 13:22:23 +0000

    WinCE patch from Sergio
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@37503 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/Gantt/kdganttgraphicsscene.cpp
+++ src/Gantt/kdganttgraphicsscene.cpp
@@ -609,8 +609,11 @@ GraphicsItem* GraphicsScene::dragSource() const
  */
 void GraphicsScene::print( QPrinter* printer, bool drawRowLabels )
 {
+//There is no printer support under wince
+#ifndef _WIN32_WCE
     QPainter painter( printer );
     doPrint( &painter, printer->pageRect(), sceneRect().left(), sceneRect().right(), printer, drawRowLabels );
+#endif
 }
 
 /*! Print part of the Gantt chart from \a start to \a end using \a printer.
@@ -625,8 +628,11 @@ void GraphicsScene::print( QPrinter* printer, bool drawRowLabels )
  */
 void GraphicsScene::print( QPrinter* printer, qreal start, qreal end, bool drawRowLabels )
 {
+//There is no printer support under wince
+#ifndef _WIN32_WCE
     QPainter painter( printer );
     doPrint( &painter, printer->pageRect(), start, end, printer, drawRowLabels );
+#endif
 }
 
 /*! Render the GanttView inside the rectangle \a target using the painter \a painter.
@@ -635,12 +641,15 @@ void GraphicsScene::print( QPrinter* printer, qreal start, qreal end, bool drawR
  */
 void GraphicsScene::print( QPainter* painter, const QRectF& _targetRect, bool drawRowLabels )
 {
+//There is no printer support under wince
+#ifndef _WIN32_WCE
     QRectF targetRect( _targetRect );
     if ( targetRect.isNull() ) {
         targetRect = sceneRect();
     }
 
     doPrint( painter, targetRect, sceneRect().left(), sceneRect().right(), 0, drawRowLabels );
+#endif
 }
 
 /*! Render the GanttView inside the rectangle \a target using the painter \a painter.
@@ -654,12 +663,15 @@ void GraphicsScene::print( QPainter* painter, const QRectF& _targetRect, bool dr
 void GraphicsScene::print( QPainter* painter, qreal start, qreal end,
                            const QRectF& _targetRect, bool drawRowLabels )
 {
+//There is no printer support under wince
+#ifndef _WIN32_WCE
     QRectF targetRect( _targetRect );
     if ( targetRect.isNull() ) {
         targetRect = sceneRect();
     }
 
     doPrint( painter, targetRect, start, end, 0, drawRowLabels );
+#endif
 }
 
 /*!\internal
@@ -668,6 +680,8 @@ void GraphicsScene::doPrint( QPainter* painter, const QRectF& targetRect,
                              qreal start, qreal end,
                              QPrinter* printer, bool drawRowLabels )
 {
+//There is no printer support under wince
+#ifndef _WIN32_WCE
     assert( painter );
     d->isPrinting = true;
 #if QT_VERSION >= QT_VERSION_CHECK(4, 4, 0)
@@ -760,6 +774,7 @@ void GraphicsScene::doPrint( QPainter* painter, const QRectF& targetRect,
     blockSignals( b );
     setSceneRect( oldScnRect );
     painter->restore();
+#endif
 }
 
 #include "moc_kdganttgraphicsscene.cpp"
