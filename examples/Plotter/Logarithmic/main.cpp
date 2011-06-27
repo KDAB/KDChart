5769c32aae1db0955ca14af9eb70ee20351029f3
Sebastian Sauer <sebastian.sauer@kdab.com>
2011-06-27 11:07:25 +0000

    Added logarithmic plotter example.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@40582 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

---
+++ examples/Plotter/Logarithmic/main.cpp
@@ -0,0 +1,98 @@
+/****************************************************************************
+** Copyright (C) 2001-2011 Klaralvdalens Datakonsult AB.  All rights reserved.
+**
+** This file is part of the KD Chart library.
+**
+** Licensees holding valid commercial KD Chart licenses may use this file in
+** accordance with the KD Chart Commercial License Agreement provided with
+** the Software.
+**
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 and version 3 as published by the
+** Free Software Foundation and appearing in the file LICENSE.GPL.txt included.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** Contact info@kdab.com if any conditions of this licensing are not
+** clear to you.
+**
+**********************************************************************/
+
+#include <QtGui>
+#include <KDChartChart>
+#include <KDChartLineDiagram>
+#include <KDChartDataValueAttributes>
+
+#include <KDChartPlotter>
+#include <KDChartLineDiagram>
+#include <KDChartCartesianAxis>
+#include <KDChartCartesianCoordinatePlane>
+#include <KDChartLegend>
+#include <QStandardItemModel>
+
+using namespace KDChart;
+
+int main( int argc, char** argv )
+{
+    QApplication app( argc, argv );
+
+    const int points = 420;
+    const double xMin = 0;
+    const double xMax = 20;
+    const double step = ( xMax - xMin ) / ( points - 1 );
+
+    QStandardItemModel model( points, 4 );
+
+    double x = xMin;
+    for( int n = 0; n < points; ++n, x += step) {
+        QModelIndex index = model.index( n, 0 );
+        model.setData( index, QVariant( x ) );
+        index = model.index( n, 1 );
+        model.setData( index, QVariant( sin( x ) * 100 ) );
+
+        index = model.index( n, 2 );
+        model.setData( index, QVariant( x ) );
+        index = model.index( n, 3 );
+        model.setData( index, QVariant( x*x*x ) );
+    }
+
+    model.setHeaderData( 0, Qt::Horizontal, QString::fromLatin1( "100 * sin(x)" ) );
+    model.setHeaderData( 1, Qt::Horizontal, QString::fromLatin1( "x^3" ) );
+
+    KDChart::Chart* chart = new KDChart::Chart();
+
+    KDChart::AbstractCartesianDiagram* diagram = new KDChart::Plotter;
+    diagram->setModel( &model );
+    chart->coordinatePlane()->replaceDiagram( diagram );
+
+    KDChart::CartesianAxis* xAxis = new KDChart::CartesianAxis( diagram );
+    KDChart::CartesianAxis* yAxis = new KDChart::CartesianAxis( diagram );
+    xAxis->setPosition( KDChart::CartesianAxis::Bottom );
+    yAxis->setPosition( KDChart::CartesianAxis::Left );
+    diagram->addAxis( xAxis );
+    diagram->addAxis( yAxis );
+
+    KDChart::Legend* legend = new KDChart::Legend( diagram, chart );
+    legend->setPosition( KDChart::Position::East );
+    legend->setAlignment( Qt::AlignCenter );
+    legend->setTitleText( "Legend" );
+    chart->addLegend( legend );
+
+    KDChart::CartesianCoordinatePlane* cart_plane = dynamic_cast<KDChart::CartesianCoordinatePlane*>(chart->coordinatePlane());
+    Q_ASSERT(cart_plane);
+
+    cart_plane->setAxesCalcModeX(KDChart::AbstractCoordinatePlane::Logarithmic);
+    cart_plane->setAxesCalcModeY(KDChart::AbstractCoordinatePlane::Logarithmic);
+
+    // Set the vertical range from 15 to 75 - with a logarithmic axis I actually get 1 to 100
+    //cart_plane->setVerticalRange(QPair<qreal,qreal>( 0.005, 1000 ) );
+
+    // Set the horizontal range from 1 to 9 - with a linear axis this works OK
+    cart_plane->setHorizontalRange(QPair<qreal,qreal>( 0.001, 100 ) );
+
+    chart->show();
+
+    return app.exec();
+}
