1e721825149534191b065a5fcc99ca60c896e492
Andreas Hartmetz <andreas.hartmetz@kdab.com>
2012-04-23 14:12:44 +0000

    Revert the part of r45670 that changed the outer dimensions of frame rects. Due to the changes some edges were cut off in examples, and in any case it was a behavior change for no really good reason. The new old behavior is not ideal either, but it should keep existing code working.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@46055 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/KDChart/KDChartAbstractAreaBase.cpp
+++ src/KDChart/KDChartAbstractAreaBase.cpp
@@ -178,13 +178,9 @@ void AbstractAreaBase::paintFrameAttributes( QPainter& painter, const QRect& rec
     const QPen oldPen( painter.pen() );
     const QBrush oldBrush( painter.brush() );
 
-    const QPen scaledPen = PrintingParameters::scalePen( attributes.pen() );
-    const int adjust = scaledPen.width() / 2;
-    const QRect adjustedRect = rect.adjusted( adjust, adjust, -adjust, -adjust );
-
-    painter.setPen( scaledPen );
+    painter.setPen( PrintingParameters::scalePen( attributes.pen() ) );
     painter.setBrush( Qt::NoBrush );
-    painter.drawRoundedRect( adjustedRect, attributes.cornerRadius(), attributes.cornerRadius() );
+    painter.drawRoundedRect( rect.adjusted( 0, 0, -1, -1 ), attributes.cornerRadius(), attributes.cornerRadius() );
 
     painter.setBrush( oldBrush );
     painter.setPen( oldPen );
@@ -195,15 +191,11 @@ void AbstractAreaBase::paintBackground( QPainter& painter, const QRect& rect )
     Q_ASSERT_X ( d != 0, "AbstractAreaBase::paintBackground()",
                 "Private class was not initialized!" );
 
-    const QPen scaledPen = PrintingParameters::scalePen( d->frameAttributes.pen() );
-    const int adjust = scaledPen.width() / 2;
-    const QRect adjustedRect = rect.adjusted( adjust, adjust, -adjust, -adjust );
-
     PainterSaver painterSaver( &painter );
 
     const qreal radius = d->frameAttributes.cornerRadius();
     QPainterPath path;
-    path.addRoundedRect( adjustedRect, radius, radius );
+    path.addRoundedRect( rect.adjusted( 0, 0, -1, -1 ), radius, radius );
     painter.setClipPath(path);
 
     paintBackgroundAttributes( painter, rect, d->backgroundAttributes );
@@ -237,12 +229,7 @@ QRect AbstractAreaBase::innerRect() const
     int right;
     int bottom;
     getFrameLeadings( left, top, right, bottom );
-    QRect rect( QPoint( 0, 0 ), areaGeometry().size() );
-    rect.adjust( left, top, -right, -bottom );
-
-    const QPen scaledPen = PrintingParameters::scalePen( d->frameAttributes.pen() );
-    const int adjust = scaledPen.width();
-    return rect.adjusted( adjust, adjust, -adjust, -adjust );
+    return QRect ( QPoint( 0, 0 ), areaGeometry().size() ).adjusted( left, top, -right, -bottom );
 }
 
 void AbstractAreaBase::positionHasChanged()
