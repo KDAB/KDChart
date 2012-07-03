KDAB/KDChart

commit c85f4f4a83779bdda993f810151f9c045a48d7c8

Land Björn's patch for KDCH-822 issue 1.
git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@45612 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

src/KDChart/KDChartAbstractDiagram.cpp
@@ -961,10 +961,10 @@ QList<QBrush> AbstractDiagram::datasetBrushes() const
     if( model() == 0 )
         return ret;
 
-    const int datasetCount = attributesModel()->columnCount(attributesModelRootIndex()) / datasetDimension();
-    for ( int dataset = 0; dataset < datasetCount; dataset++ )
+    const int datasetCount = attributesModel()->columnCount( attributesModelRootIndex() );
+    for ( int dataset = 0; dataset < datasetCount; dataset += datasetDimension() ) {
         ret << brush( dataset );
-
+    }
     return ret;
 }
 
@@ -974,10 +974,10 @@ QList<QPen> AbstractDiagram::datasetPens() const
     if( model() == 0 )
         return ret;
 
-    const int datasetCount = attributesModel()->columnCount(attributesModelRootIndex()) / datasetDimension();
-    for ( int dataset = 0; dataset < datasetCount; dataset++ )
+    const int datasetCount = attributesModel()->columnCount( attributesModelRootIndex() );
+    for ( int dataset = 0; dataset < datasetCount; dataset += datasetDimension() ) {
         ret << pen( dataset );
-
+    }
     return ret;
 }
 
@@ -987,10 +987,10 @@ QList<MarkerAttributes> AbstractDiagram::datasetMarkers() const
     if( model() == 0 )
         return ret;
 
-    const int datasetCount = attributesModel()->columnCount(attributesModelRootIndex()) / datasetDimension();
-    for ( int dataset = 0; dataset < datasetCount; dataset++ )
+    const int datasetCount = attributesModel()->columnCount( attributesModelRootIndex() );
+    for ( int dataset = 0; dataset < datasetCount; dataset += datasetDimension() ) {
         ret << dataValueAttributes( dataset ).markerAttributes();
-
+    }
     return ret;
 }
 

commit 64b1025f9f92c8163030da3c6241d8e8b31c1d29

Fix wrong legend colors (and other legend properties).
That was a regression introduced in r45612, to fix an issue in
KDCH-822. I think the real problem is that, with two-dimensional data,
an odd number of columns simply makes no sense. We should rather do a
sanity check and ignore the last column entirely. That still needs
to be done.

git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@46911 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

src/KDChart/KDChartAbstractDiagram.cpp
@@ -916,29 +916,25 @@ QStringList AbstractDiagram::itemRowLabels() const
 QStringList AbstractDiagram::datasetLabels() const
 {
     QStringList ret;
-    if( model() == 0 )
+    if ( !model() ) {
         return ret;
-
-    const int columnCount = attributesModel()->columnCount(attributesModelRootIndex());
-    const int dataSetCount =  datasetDimension();
-    const int loopEnd = columnCount / dataSetCount;
-    for( int i = 0; i < loopEnd; ++i )
-    {
+    }
+    const int datasetCount = d->datasetCount();
+    for ( int i = 0; i < datasetCount; ++i ) {
         ret << d->datasetAttrs( i, Qt::DisplayRole ).toString();
     }
-
     return ret;
 }
 
 QList<QBrush> AbstractDiagram::datasetBrushes() const
 {
     QList<QBrush> ret;
-    if( model() == 0 )
+    if ( !model() ) {
         return ret;
-
-    const int datasetCount = attributesModel()->columnCount( attributesModelRootIndex() );
-    for ( int dataset = 0; dataset < datasetCount; dataset += datasetDimension() ) {
-        ret << brush( dataset );
+    }
+    const int datasetCount = d->datasetCount();
+    for ( int i = 0; i < datasetCount; ++i ) {
+        ret << brush( i );
     }
     return ret;
 }
@@ -946,12 +942,12 @@ QList<QBrush> AbstractDiagram::datasetBrushes() const
 QList<QPen> AbstractDiagram::datasetPens() const
 {
     QList<QPen> ret;
-    if( model() == 0 )
+    if ( !model() ) {
         return ret;
-
-    const int datasetCount = attributesModel()->columnCount( attributesModelRootIndex() );
-    for ( int dataset = 0; dataset < datasetCount; dataset += datasetDimension() ) {
-        ret << pen( dataset );
+    }
+    const int datasetCount = d->datasetCount();
+    for ( int i = 0; i < datasetCount; ++i ) {
+        ret << pen( i );
     }
     return ret;
 }
@@ -959,12 +955,12 @@ QList<QPen> AbstractDiagram::datasetPens() const
 QList<MarkerAttributes> AbstractDiagram::datasetMarkers() const
 {
     QList<MarkerAttributes> ret;
-    if( model() == 0 )
+    if ( !model() ) {
         return ret;
-
-    const int datasetCount = attributesModel()->columnCount( attributesModelRootIndex() );
-    for ( int dataset = 0; dataset < datasetCount; dataset += datasetDimension() ) {
-        ret << dataValueAttributes( dataset ).markerAttributes();
+    }
+    const int datasetCount = d->datasetCount();
+    for ( int i = 0; i < datasetCount; ++i ) {
+        ret << dataValueAttributes( i ).markerAttributes();
     }
     return ret;
 }

src/KDChart/KDChartAbstractDiagram_p.h
@@ -151,6 +151,11 @@ namespace KDChart {
                                  bool justCalculateRect=false,
                                  QRectF* cumulatedBoundingRect=0 );
 
+        inline int datasetCount() const
+        {
+            return attributesModel->columnCount( attributesModelRootIndex ) / datasetDimension;
+        }
+
         virtual QModelIndex indexAt( const QPoint& point ) const;
 
         QModelIndexList indexesAt( const QPoint& point ) const;
@@ -223,7 +228,7 @@ namespace KDChart {
 
 
     class LineAttributesInfo {
-        public :
+    public:
         LineAttributesInfo();
         LineAttributesInfo( const QModelIndex& _index, const QPointF& _value, const QPointF& _nextValue );
 