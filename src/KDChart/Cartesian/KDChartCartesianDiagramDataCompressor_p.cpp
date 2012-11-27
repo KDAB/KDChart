KDAB/KDChart

commit 027c5198fb3ea77ac8bbe319241e233501fd5700

Fix a failing index range assertion by unscrewing the indexing.
qRound() was a worse choice than floor() here.
For example, with 1.01 indexesPerPixel() and 101 indexes / 100
CachePositions, on CachePosition 99 the previous algorithm iterated
from index 100 to 101. But 101 is invalid.
It also makes sense to stop where the next CachePosition starts, using
the same formula for start of current CachePosition and start of next
CachePositon, which marks the end of the current.
A side effect of this change is that the number of data points
returned per CachePosition varies, which is good for performance and
bad for visual consistency.

git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@48748 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

src/KDChart/Cartesian/KDChartCartesianDiagramDataCompressor_p.cpp
@@ -569,12 +569,15 @@ QModelIndexList CartesianDiagramDataCompressor::mapToModel( const CachePosition&
         indexes << m_model->index( position.row, position.column * 2 + 1, m_rootIndex ); // checked
     } else {
         // assumption: indexes per column == 1
+        Q_ASSERT( position.column < m_model->columnCount() );
         const qreal ipp = indexesPerPixel();
-        for ( int i = 0; i < ipp; ++i ) {
-            int row = qRound( position.row * ipp ) + i;
-            int col = position.column;
-            Q_ASSERT( row < m_model->rowCount() && col < m_model->columnCount() );
-            const QModelIndex index = m_model->index( row, col, m_rootIndex ); // checked
+        const int baseRow = floor( position.row * ipp );
+        // the following line needs to work for the last row(s), too...
+        const int endRow = floor( ( position.row + 1 ) * ipp );
+        qDebug() << Q_FUNC_INFO << ipp << position.row << baseRow << endRow;
+        for ( int row = 0; row < endRow; ++row ) {
+            Q_ASSERT( row < m_model->rowCount() );
+            const QModelIndex index = m_model->index( row, position.column, m_rootIndex );
             if ( index.isValid() ) {
                 indexes << index;
             }

commit 7b97e7d6df3ffcda1c1f7e91a376c09a4f7b60e3

Fix a stupid indexing bug introduced in r48748.
git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@48851 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

src/KDChart/Cartesian/KDChartCartesianDiagramDataCompressor_p.cpp
@@ -574,7 +574,7 @@ QModelIndexList CartesianDiagramDataCompressor::mapToModel( const CachePosition&
         const int baseRow = floor( position.row * ipp );
         // the following line needs to work for the last row(s), too...
         const int endRow = floor( ( position.row + 1 ) * ipp );
-        for ( int row = 0; row < endRow; ++row ) {
+        for ( int row = baseRow; row < endRow; ++row ) {
             Q_ASSERT( row < m_model->rowCount() );
             const QModelIndex index = m_model->index( row, position.column, m_rootIndex );
             if ( index.isValid() ) {