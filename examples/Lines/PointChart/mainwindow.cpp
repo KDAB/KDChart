9c595b4e3fcd24258a2fdc996cba9644260cd145
Andreas Hartmetz <andreas.hartmetz@kdab.com>
2013-03-26 19:43:22 +0000

    Fix KDCHDEV-816, same bugs as in two or three other examples so far.
    I don't think we need the "map" marker style and all its boilerplate
    in every demo, so I've simply removed it, which fixes KDCHDEV-816.
    Additionally, I've fixed another thing was also wrong in the other
    similar demos: Data value text and marker settings interfering with
    each other.
    There is some code cleanup in this, too.
    
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@50141 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/Lines/PointChart/mainwindow.cpp
+++ examples/Lines/PointChart/mainwindow.cpp
@@ -79,18 +79,15 @@ void MainWindow::on_lineTypeCB_currentIndexChanged( const QString & text )
 void MainWindow::on_paintValuesCB_toggled( bool checked )
 {
     const int colCount = m_lines->model()->columnCount(m_lines->rootIndex());
-    for ( int iColumn = 0; iColumn<colCount; ++iColumn ) {
+    for ( int iColumn = 0; iColumn < colCount; ++iColumn ) {
         DataValueAttributes a( m_lines->dataValueAttributes( iColumn ) );
         QBrush brush( m_lines->brush( iColumn ) );
         TextAttributes ta( a.textAttributes() );
         ta.setRotation( 0 );
         ta.setFont( QFont( "Comic" ) );
         ta.setPen( QPen( brush.color() ) );
+        ta.setVisible( checked );
 
-        if ( checked )
-            ta.setVisible( true );
-        else
-            ta.setVisible( false );
         a.setVisible( true );
         a.setTextAttributes( ta );
         m_lines->setDataValueAttributes( iColumn, a );
@@ -112,77 +109,59 @@ void MainWindow::on_paintLinesCB_toggled(  bool checked )
         else
             m_lines->setPen( iColumn,  Qt::NoPen );
     }
-        m_chart->update();
+    m_chart->update();
 }
 
 
 void MainWindow::on_paintMarkersCB_toggled( bool checked )
 {
-    // set up a map with different marker styles
-    MarkerAttributes::MarkerStylesMap map;
-    map.insert( 0, MarkerAttributes::MarkerSquare );
-    map.insert( 1, MarkerAttributes::MarkerCircle );
-    map.insert( 2, MarkerAttributes::MarkerRing );
-    map.insert( 3, MarkerAttributes::MarkerCross );
-    map.insert( 4, MarkerAttributes::MarkerDiamond );
-
-
     const int colCount = m_lines->model()->columnCount(m_lines->rootIndex());
     for ( int iColumn = 0; iColumn<colCount; ++iColumn ) {
         DataValueAttributes dva( m_lines->dataValueAttributes( iColumn ) );
         QBrush lineBrush( m_lines->brush( iColumn ) );
         TextAttributes ta ( dva.textAttributes() );
-        if (  paintValuesCB->isChecked() )
-            ta.setVisible(  true );
-        else
-            ta.setVisible( false );
+        ta.setVisible( paintValuesCB->isChecked() );
         MarkerAttributes ma( dva.markerAttributes() );
-        ma.setMarkerStylesMap( map );
         ma.setMarkerSize( QSize( markersWidthSB->value(),
                                  markersHeightSB->value() ) );
 
         switch ( markersStyleCB->currentIndex() ) {
         case 0:
-            break;
-        case 1:
             ma.setMarkerStyle( MarkerAttributes::MarkerCircle );
             break;
-        case 2:
+        case 1:
             ma.setMarkerStyle( MarkerAttributes::MarkerSquare );
             break;
-        case 3:
+        case 2:
             ma.setMarkerStyle( MarkerAttributes::MarkerDiamond );
             break;
-        case 4:
+        case 3:
             ma.setMarkerStyle( MarkerAttributes::Marker1Pixel );
             break;
-        case 5:
+        case 4:
             ma.setMarkerStyle( MarkerAttributes::Marker4Pixels );
             break;
-        case 6:
+        case 5:
             ma.setMarkerStyle( MarkerAttributes::MarkerRing );
             break;
-        case 7:
+        case 6:
             ma.setMarkerStyle( MarkerAttributes::MarkerCross );
             break;
-        case 8:
+        case 7:
             ma.setMarkerStyle( MarkerAttributes::MarkerFastCross );
             break;
-        case 9:
+        case 8:
             ma.setMarkerStyle( MarkerAttributes::PainterPathMarker );
         }
 
         QPen markerPen( lineBrush.color() );
         ma.setPen( markerPen );
-        ma.setVisible(  true );
+        ma.setVisible( checked );
         ma.setCustomMarkerPath( *path );
         dva.setTextAttributes( ta );
         dva.setMarkerAttributes( ma );
+        dva.setVisible( true );
 
-        if (  checked )
-            dva.setVisible( true );
-        else
-            dva.setVisible(  false );
         m_lines->setDataValueAttributes( iColumn, dva );
     }
 
@@ -193,22 +172,18 @@ void MainWindow::on_paintMarkersCB_toggled( bool checked )
 void MainWindow::on_markersStyleCB_currentIndexChanged( const QString & text )
 {
     Q_UNUSED( text );
-    if ( paintMarkersCB->isChecked() )
-        on_paintMarkersCB_toggled( true );
+    on_paintMarkersCB_toggled( paintMarkersCB->isChecked() );
 }
 
 
 void MainWindow::on_markersWidthSB_valueChanged( int i )
 {
-    Q_UNUSED( i );
-    markersHeightSB->setValue( markersWidthSB->value() );
-    if ( paintMarkersCB->isChecked() )
-        on_paintMarkersCB_toggled( true );
+    markersHeightSB->setValue( i );
+    on_paintMarkersCB_toggled( paintMarkersCB->isChecked() );
 }
 
-void MainWindow::on_markersHeightSB_valueChanged( int /*i*/ )
+void MainWindow::on_markersHeightSB_valueChanged( int i )
 {
-    markersWidthSB->setValue( markersHeightSB->value() );
-    if ( paintMarkersCB->isChecked() )
-        on_paintMarkersCB_toggled( true );
+    markersWidthSB->setValue( i );
+    on_paintMarkersCB_toggled( paintMarkersCB->isChecked() );
 }
