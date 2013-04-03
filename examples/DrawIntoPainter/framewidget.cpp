ab7d065f75fa4920d18720c985be84f442afe2f3
Andreas Hartmetz <andreas.hartmetz@kdab.com>
2013-04-03 17:36:38 +0000

    Formatting.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@50230 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- examples/DrawIntoPainter/framewidget.cpp
+++ examples/DrawIntoPainter/framewidget.cpp
@@ -44,37 +44,37 @@ void FrameWidget::setChart( KDChart::Chart* chart )
 
 void FrameWidget::paintEvent( QPaintEvent* e )
 {
-    if( !mChart ){
+    if ( !mChart ) {
         QWidget::paintEvent( e );
-    }else{
+    } else {
         QPainter painter( this );
 
-        const int wid=64;
-        const QRect  r( rect() );
-        const QPen   oldPen(   painter.pen() );
+        const int wid = 64;
+        const QRect r( rect() );
+        const QPen oldPen( painter.pen() );
         const QBrush oldBrush( painter.brush() );
         // paint below the chart
-        painter.setPen( QPen(Qt::NoPen) );
-        painter.setBrush( QBrush(QColor(0xd0,0xd0,0xff)) );
-        painter.drawEllipse(r.left(),                 r.top(),                  wid,wid);
-        painter.drawEllipse(r.left()+r.width()-wid-1, r.top(),                  wid,wid);
-        painter.drawEllipse(r.left(),                 r.top()+r.height()-wid-1, wid,wid);
-        painter.drawEllipse(r.left()+r.width()-wid-1, r.top()+r.height()-wid-1, wid,wid);
+        painter.setPen( QPen( Qt::NoPen ) );
+        painter.setBrush( QBrush( QColor( 0xd0, 0xd0, 0xff ) ) );
+        painter.drawEllipse( r.left(),                       r.top(),                        wid, wid );
+        painter.drawEllipse( r.left() + r.width() - wid - 1, r.top(),                        wid, wid );
+        painter.drawEllipse( r.left(),                       r.top() + r.height() - wid - 1, wid, wid );
+        painter.drawEllipse( r.left() + r.width() - wid - 1, r.top() + r.height() - wid - 1, wid, wid );
         painter.setBrush( oldBrush );
-        painter.setPen(   oldPen );
+        painter.setPen( oldPen );
         // paint the chart
-        mChart->paint(&painter, QRect( r.left() + wid / 2, r.top() + wid / 2,
-                                       r.width() - wid,    r.height() - wid ) );
+        mChart->paint( &painter, QRect( r.left() + wid / 2, r.top() + wid / 2,
+                                        r.width() - wid,    r.height() - wid ) );
         // paint over the chart
-        painter.setPen( QPen(Qt::NoPen) );
-        painter.setBrush( QBrush(QColor(0xd0,0xff,0xff)) );
-        const int wid2=40;
-        const int gap=(wid-wid2)/2;
-        painter.drawEllipse(r.left()+gap,                 r.top()+gap,                  wid2,wid2);
-        painter.drawEllipse(r.left()+r.width()-wid+gap-1, r.top()+gap,                  wid2,wid2);
-        painter.drawEllipse(r.left()+gap,                 r.top()+r.height()-wid+gap-1, wid2,wid2);
-        painter.drawEllipse(r.left()+r.width()-wid+gap-1, r.top()+r.height()-wid+gap-1, wid2,wid2);
+        painter.setPen( QPen( Qt::NoPen ) );
+        painter.setBrush( QBrush( QColor( 0xd0, 0xff, 0xff ) ) );
+        const int wid2 = 40;
+        const int gap = ( wid - wid2 ) / 2;
+        painter.drawEllipse( r.left() + gap,                       r.top() + gap,                        wid2, wid2 );
+        painter.drawEllipse( r.left() + r.width() - wid + gap - 1, r.top() + gap,                        wid2, wid2 );
+        painter.drawEllipse( r.left() + gap,                       r.top() + r.height() - wid + gap - 1, wid2, wid2 );
+        painter.drawEllipse( r.left() + r.width() - wid + gap - 1, r.top() + r.height() - wid + gap - 1, wid2, wid2 );
         painter.setBrush( oldBrush );
-        painter.setPen(   oldPen );
+        painter.setPen( oldPen );
     }
 }
