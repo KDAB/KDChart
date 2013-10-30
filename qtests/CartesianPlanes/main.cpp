b3c2ee80780a7cd6f3e5fafb964e247ac3df982a
Andreas Hartmetz <andreas.hartmetz@kdab.com>
2013-10-30 22:56:07 +0000

    Make the data range tests useful.
    Five major improvements:
    - Don't reuse the same objects for all test cases
    - Use test input data that is easy to eyeball by humans
    - Programmatically derive the expected results from the input data
    - Test not only (1d) BarDiagram, but also (2d) Plotter
    - Test that auto-range is correct when enabled, not just that it's
      inactive when disabled.
    
    Also de-inline the test functions.
    
    A remaining problem is that I'm not completely sure how auto-range
    *should* work, so the tests can fail with certain input data, as I've
    found by trying.
    
    This fixes the failing tests.
    
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@53027 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- qtests/CartesianPlanes/main.cpp
+++ qtests/CartesianPlanes/main.cpp
@@ -26,15 +26,11 @@
 #include <QPair>
 #include <QString>
 #include <KDChartChart>
-#include <KDChartGlobal>
 #include <KDChartCartesianCoordinatePlane>
-#include <KDChartAbstractCoordinatePlane>
 #include <KDChartBarDiagram>
-#include <KDChartLineDiagram>
+#include <KDChartPlotter>
 #include <KDChartGridAttributes>
-#include <KDChartLegend>
 
-#include <TableModel.h>
 
 using namespace KDChart;
 
@@ -46,172 +42,264 @@ template <> inline char *toString(const QPointF &p) {
 }
 #endif
 
-class TestCartesianPlanes: public QObject {
+class NumericDataModel : public QStandardItemModel
+{
+    Q_OBJECT
+public:
+    void setYValues( const QList< qreal > &values )
+    {
+        min = QPointF( 0, 1e9 );
+        max = QPointF( values.size() - 1, -1e9 );
+        setRowCount( values.size() );
+        setColumnCount( 1 );
+        for ( int i = 0; i < values.size(); i++ ) {
+            min.setY( qMin( min.y(), values[ i ] ) );
+            max.setY( qMax( max.y(), values[ i ] ) );
+
+            QModelIndex idx = index( i, 0 );
+            QStandardItemModel::setData( idx, values[ i ] );
+        }
+    }
+
+    void setXyValues( const QList< QPointF > &values )
+    {
+        min = QPointF( 1e9, 1e9 );
+        max = QPointF( -1e9, -1e9 );
+        setRowCount( values.size() );
+        setColumnCount( 2 );
+        for ( int i = 0; i < values.size(); i++ ) {
+            min.setX( qMin( min.x(), values[ i ].x() ) );
+            max.setX( qMax( max.x(), values[ i ].x() ) );
+            min.setY( qMin( min.y(), values[ i ].y() ) );
+            max.setY( qMax( max.y(), values[ i ].y() ) );
+
+            QModelIndex idx = index( i, 0 );
+            QStandardItemModel::setData( idx, values[ i ].x() );
+            idx = index( i, 1 );
+            QStandardItemModel::setData( idx, values[ i ].y() );
+        }
+    }
+
+    QPointF min;
+    QPointF max;
+};
+
+class TestCartesianPlanes : public QObject {
     Q_OBJECT
 private slots:
 
-    void initTestCase()
+    void init();
+    void cleanup();
+    void testIntialOwnership();
+    void testDiagramOwnership();
+    void testIsometricScalingSettings();
+    void testZoomFactorsSettings();
+    void testRangeSettingsBars();
+    void testRangeSettingsPlotter();
+    void testGlobalGridAttributesSettings();
+    void testGridAttributesSettings();
+    void testAxesCalcModesSettings();
+
+private:
+    void doTestRangeSettings( AbstractCartesianDiagram *diagram, const QPointF &min, const QPointF &max );
+
+    Chart *m_chart;
+    BarDiagram *m_bars;
+    Plotter *m_plotter;
+    CartesianCoordinatePlane *m_plane;
+    NumericDataModel *m_model;
+
+};
+
+void TestCartesianPlanes::init()
+{
+    m_chart = new Chart( 0 );
+    m_model = new NumericDataModel();
+    m_model->setParent( m_chart );
+    m_bars = new BarDiagram( m_chart );
+    m_bars->setModel( m_model );
+    m_plotter = new Plotter( m_chart );
+    m_plotter->setModel( m_model );
+    m_plane = new CartesianCoordinatePlane( m_chart );
+    m_chart->addCoordinatePlane( m_plane );
+    m_plane->setReferenceCoordinatePlane( m_chart->coordinatePlane() );
+    qDebug() << m_plotter->datasetDimension();
+}
+
+void TestCartesianPlanes::cleanup()
+{
+    delete m_chart;
+}
+
+
+void TestCartesianPlanes::testIntialOwnership()
+{
+    AbstractCoordinatePlane *plane = m_chart->coordinatePlane();
+    QCOMPARE( m_plane->referenceCoordinatePlane(), m_chart->coordinatePlane() );
+    m_chart->takeCoordinatePlane( 0 );
+    delete plane;
+    QCOMPARE( m_plane->referenceCoordinatePlane(), (AbstractCoordinatePlane*)0 );
+}
+
+void TestCartesianPlanes::testDiagramOwnership()
+{
+    m_plane->addDiagram( m_bars );
+    QCOMPARE( m_plane->diagrams().size(), 1 );
+    m_plane->addDiagram( m_plotter );
+    QCOMPARE( m_plane->diagrams().size(), 2 );
+    QCOMPARE( dynamic_cast< BarDiagram * >( m_plane->diagram() ), m_bars );
+    m_plane->takeDiagram( m_bars );
+    QCOMPARE( m_plane->diagrams().size(), 1 );
+    QCOMPARE( dynamic_cast< Plotter * >( m_plane->diagram() ), m_plotter );
+    m_plane->replaceDiagram( m_bars,  m_plotter );
+    QCOMPARE( m_plane->diagrams().size(), 1 );
+    QCOMPARE( dynamic_cast< BarDiagram * >( m_plane->diagram() ), m_bars );
+    m_plane->takeDiagram( m_bars );
+    QCOMPARE( m_plane->diagrams().size(), 0 );
+    delete m_bars;
+}
+
+void TestCartesianPlanes::testIsometricScalingSettings()
+{
+    QVERIFY( m_plane->doesIsometricScaling() == false );
+    m_plane->setIsometricScaling( true );
+    QVERIFY( m_plane->doesIsometricScaling() == true );
+}
+
+void TestCartesianPlanes::testZoomFactorsSettings()
+{
+    QCOMPARE( m_plane->zoomFactorX(), 1.0 );
+    QCOMPARE( m_plane->zoomFactorY(), 1.0 );
+    QCOMPARE( m_plane->zoomCenter(), QPointF( 0.5, 0.5 ) );
+    m_plane->setZoomFactorX( 1.5 );
+    m_plane->setZoomFactorY( 1.5 );
+    m_plane->setZoomCenter( QPointF( 1.0, 1.0 ) );
+    QCOMPARE( m_plane->zoomFactorX(), 1.5 );
+    QCOMPARE( m_plane->zoomFactorY(), 1.5 );
+    QCOMPARE( m_plane->zoomCenter(), QPointF( 1.0, 1.0 ) );
+}
+
+void TestCartesianPlanes::doTestRangeSettings( AbstractCartesianDiagram *diagram,
+                                               const QPointF &min, const QPointF &max )
+{
+    m_plane->addDiagram( diagram );
+    // the range is null when auto adjustment is turned off - check that...
+    m_plane->setAutoAdjustHorizontalRangeToData( 100 );
+    m_plane->setAutoAdjustVerticalRangeToData( 100 );
+
     {
-        m_chart = new Chart(0);
-        m_tableModel = new TableModel( this );
-        m_tableModel->loadFromCSV( ":/data" );
-        m_bars = new BarDiagram();
-        m_bars->setModel( m_tableModel );
-        m_lines = new LineDiagram();
-        m_lines->setModel( m_tableModel );
-        m_plane = new CartesianCoordinatePlane();
-        m_chart->addCoordinatePlane( m_plane );
-        m_plane->setReferenceCoordinatePlane( m_chart->coordinatePlane() );
+        const QPair< qreal, qreal > hrange = m_plane->horizontalRange();
+        const QPair< qreal, qreal > vrange = m_plane->verticalRange();
+        QCOMPARE( hrange.first, qreal( 0.0 ) );
+        QCOMPARE( hrange.second, qreal( 0.0 ) );
+        QCOMPARE( vrange.first, qreal( 0.0 ) );
+        QCOMPARE( vrange.second, qreal( 0.0 ) );
     }
 
-    void testIntialOwnership()
+    // now check that auto adjustment works when enabled
+    m_plane->setAutoAdjustHorizontalRangeToData( 67 );
+    m_plane->setAutoAdjustVerticalRangeToData( 67 );
     {
-        AbstractCoordinatePlane *plane = m_chart->coordinatePlane();
-        QCOMPARE( m_plane->referenceCoordinatePlane(), m_chart->coordinatePlane() );
-        m_chart->takeCoordinatePlane( 0 );
-        delete plane;
-        QCOMPARE( m_plane->referenceCoordinatePlane(), (AbstractCoordinatePlane*)0 );
+        const QPair< qreal, qreal > hrange = m_plane->horizontalRange();
+        const QPair< qreal, qreal > vrange = m_plane->verticalRange();
+        QCOMPARE( hrange.first, min.x() );
+        QCOMPARE( hrange.second, max.x() );
+        QCOMPARE( vrange.first, min.y() );
+        QCOMPARE( vrange.second, max.y() );
     }
 
-    void testDiagramOwnership()
     {
-        m_plane->addDiagram(  m_bars );
-        QCOMPARE( m_plane->diagrams().size(),  1 );
-        m_plane->addDiagram(  m_lines );
-        QCOMPARE( m_plane->diagrams().size(),  2 );
-        QCOMPARE( dynamic_cast< BarDiagram * >(m_plane->diagram()),  m_bars );
-        m_plane->takeDiagram( m_bars );
-        QCOMPARE( m_plane->diagrams().size(),  1 );
-        QCOMPARE( dynamic_cast< LineDiagram * >(m_plane->diagram()),  m_lines );
-        m_plane->replaceDiagram( m_bars,  m_lines );
-        QCOMPARE( m_plane->diagrams().size(),  1 );
-        QCOMPARE( dynamic_cast< BarDiagram * >(m_plane->diagram()),  m_bars );
-        m_plane->takeDiagram( m_bars );
-        QCOMPARE( m_plane->diagrams().size(),  0 );
-        delete m_bars;
-    }
-
-      void testIsometricScalingSettings()
-    {
-        QVERIFY( m_plane->doesIsometricScaling() == false );
-        m_plane->setIsometricScaling( true );
-        QVERIFY( m_plane->doesIsometricScaling() == true );
-    }
-
-
-    void testZoomFactorsSettings()
-    {
-        QCOMPARE( m_plane->zoomFactorX(),  1.0 );
-        QCOMPARE( m_plane->zoomFactorY(),  1.0 );
-        QCOMPARE( m_plane->zoomCenter(), QPointF( 0.5, 0.5 ) );
-        m_plane->setZoomFactorX( 1.5 );
-        m_plane->setZoomFactorY( 1.5 );
-        m_plane->setZoomCenter( QPointF ( 1.0, 1.0 ) );
-        QCOMPARE( m_plane->zoomFactorX(),  1.5 );
-        QCOMPARE( m_plane->zoomFactorY(),  1.5 );
-        QCOMPARE( m_plane->zoomCenter(),  QPointF( 1.0, 1.0 ) );
-    }
-
-
-    void testRangeSettings()
-    {
-        initTestCase ();
-        const QPair< qreal,  qreal> expectedHrange( 0, 30 );
-        const QPair< qreal,  qreal> expectedVrange( 34, 45 );
-        m_plane->addDiagram(  m_bars );
-        // note: the range is only Null, if auto-adjusting is turned off!
-        m_plane->setAutoAdjustHorizontalRangeToData(100);
-        m_plane->setAutoAdjustVerticalRangeToData(100);
-        //qDebug() << m_plane->horizontalRange() << m_plane->verticalRange();
-        const QPair<qreal, qreal> hrange = m_plane->horizontalRange();
-        const QPair<qreal, qreal> vrange = m_plane->verticalRange();
-        
-        //use single-component QCOMPARE instead of QCOMPARE on the pairs, to make sure qFuzzyCompare is used
-        QCOMPARE( hrange.first + 1, expectedHrange.first + 1 ); //avoid comparison against 0, which doesn't work with qFuzzyCompare
-        QCOMPARE( hrange.second, expectedHrange.second );
-        QCOMPARE( vrange.first, expectedVrange.first );
-        QCOMPARE( vrange.second, expectedVrange.second );
-        QPair< qreal,  qreal> hboundaries( m_bars->dataBoundaries().first.x(),
-                                           m_bars->dataBoundaries().second.x() );
-        QPair< qreal,  qreal> vboundaries( int(m_bars->dataBoundaries().first.y()+0.5),
-                                           int(m_bars->dataBoundaries().second.y()+0.5) );
+        QPair< qreal, qreal> hboundaries( diagram->dataBoundaries().first.x(),
+                                          diagram->dataBoundaries().second.x() );
+        QPair< qreal, qreal> vboundaries( int( diagram->dataBoundaries().first.y() + 0.5 ),
+                                          int( diagram->dataBoundaries().second.y() + 0.5 ) );
         m_plane->setHorizontalRange( hboundaries );
         m_plane->setVerticalRange( vboundaries );
-        const QPair<qreal, qreal> newhb = m_plane->horizontalRange();
-        const QPair<qreal, qreal> newvb = m_plane->verticalRange();
+        const QPair< qreal, qreal > newhb = m_plane->horizontalRange();
+        const QPair< qreal, qreal > newvb = m_plane->verticalRange();
         QCOMPARE( newhb.first, hboundaries.first );
         QCOMPARE( newhb.second, hboundaries.second );
         QCOMPARE( newvb.first, vboundaries.first );
         QCOMPARE( newvb.second, vboundaries.second );
     }
+}
 
-    void testGlobalGridAttributesSettings()
-    {
-        GridAttributes ga = m_plane->globalGridAttributes();
-        QVERIFY( ga.isGridVisible() == true );
-        ga.setGridVisible(  false );
-        m_plane->setGlobalGridAttributes(  ga );
-        QVERIFY( m_plane->globalGridAttributes().isGridVisible() == false );
-        //reset to normal
-        ga.setGridVisible(  true );
-        QVERIFY( m_plane->globalGridAttributes().isGridVisible() == false );
-        m_plane->setGlobalGridAttributes(  ga );
-        QVERIFY( m_plane->globalGridAttributes().isGridVisible() == true );
-    }
+void TestCartesianPlanes::testRangeSettingsBars()
+{
+    QList< qreal > points;
+    points << 40 << 45 << 42 << 34 << 34;
+    m_model->setYValues( points );
+    // data point "0" is shown at "0.5" in bar diagrams, which requires some data range hackery.
+    // we correct for that like so:
+    m_model->max.rx() += 1;
 
-    void testGridAttributesSettings()
-    {
-        GridAttributes gh = m_plane->gridAttributes( Qt::Horizontal );
-        GridAttributes gv = m_plane->gridAttributes( Qt::Vertical );
-        QVERIFY( gh.isGridVisible() == true );
-        QVERIFY( gv.isGridVisible() == true );
-        gh.setGridVisible( false );
-        m_plane->setGridAttributes( Qt::Horizontal, gh );
-        QVERIFY( m_plane->hasOwnGridAttributes( Qt::Horizontal ) == true );
-        QVERIFY( m_plane->hasOwnGridAttributes( Qt::Vertical ) == false );
-        QVERIFY( m_plane->gridAttributes( Qt::Horizontal ).isGridVisible() == false );
-        QVERIFY( m_plane->gridAttributes( Qt::Vertical ).isGridVisible() == true );
-        gv.setGridVisible( false );
-        m_plane->setGridAttributes( Qt::Vertical, gv );
-        QVERIFY( m_plane->hasOwnGridAttributes( Qt::Horizontal ) == true );
-        QVERIFY( m_plane->hasOwnGridAttributes( Qt::Vertical ) == true );
-        QVERIFY( m_plane->gridAttributes( Qt::Horizontal ).isGridVisible() == false );
-        QVERIFY( m_plane->gridAttributes( Qt::Vertical ).isGridVisible() == false );
-        m_plane->resetGridAttributes( Qt::Horizontal );
-        m_plane->resetGridAttributes( Qt::Vertical );
-        QVERIFY( m_plane->gridAttributes( Qt::Horizontal ).isGridVisible() == true );
-        QVERIFY( m_plane->gridAttributes( Qt::Vertical ).isGridVisible() == true );
-        QVERIFY( m_plane->hasOwnGridAttributes( Qt::Horizontal ) == false );
-        QVERIFY( m_plane->hasOwnGridAttributes( Qt::Vertical ) == false );
-    }
+    doTestRangeSettings( m_bars, m_model->min, m_model->max );
+}
 
-    void testAxesCalcModesSettings()
-    {
-        QCOMPARE( m_plane->axesCalcModeX(), AbstractCoordinatePlane::Linear );
-        QCOMPARE( m_plane->axesCalcModeY(), AbstractCoordinatePlane::Linear );
-        m_plane->setAxesCalcModes(  AbstractCoordinatePlane::Logarithmic );
-        QCOMPARE( m_plane->axesCalcModeX(), AbstractCoordinatePlane::Logarithmic );
-        QCOMPARE( m_plane->axesCalcModeY(), AbstractCoordinatePlane::Logarithmic );
-        m_plane->setAxesCalcModeX(  AbstractCoordinatePlane::Linear );
-        QCOMPARE( m_plane->axesCalcModeX(), AbstractCoordinatePlane::Linear );
-        QCOMPARE( m_plane->axesCalcModeY(), AbstractCoordinatePlane::Logarithmic );
-        m_plane->setAxesCalcModeY(  AbstractCoordinatePlane::Linear );
-        QCOMPARE( m_plane->axesCalcModeX(), AbstractCoordinatePlane::Linear );
-        QCOMPARE( m_plane->axesCalcModeY(), AbstractCoordinatePlane::Linear );
-    }
+void TestCartesianPlanes::testRangeSettingsPlotter()
+{
+    QList< QPointF > points;
+    points << QPointF( 0.0, 40.0 ) << QPointF( 1.0, 45.0 ) << QPointF( 2.0, 42.0 )
+           << QPointF( 3.0, 34.0 ) << QPointF( 4.0, 34.0 );
+    m_model->setXyValues( points );
+    doTestRangeSettings( m_plotter, m_model->min, m_model->max );
+}
 
-    void cleanupTestCase()
-    {
-    }
+void TestCartesianPlanes::testGlobalGridAttributesSettings()
+{
+    GridAttributes ga = m_plane->globalGridAttributes();
+    QVERIFY( ga.isGridVisible() == true );
+    ga.setGridVisible( false );
+    m_plane->setGlobalGridAttributes( ga );
+    QVERIFY( m_plane->globalGridAttributes().isGridVisible() == false );
+    //reset to normal
+    ga.setGridVisible( true );
+    QVERIFY( m_plane->globalGridAttributes().isGridVisible() == false );
+    m_plane->setGlobalGridAttributes( ga );
+    QVERIFY( m_plane->globalGridAttributes().isGridVisible() == true );
+}
 
-private:
-    Chart *m_chart;
-    BarDiagram *m_bars;
-    LineDiagram *m_lines;
-    CartesianCoordinatePlane *m_plane;
-    TableModel *m_tableModel;
+void TestCartesianPlanes::testGridAttributesSettings()
+{
+    GridAttributes gh = m_plane->gridAttributes( Qt::Horizontal );
+    GridAttributes gv = m_plane->gridAttributes( Qt::Vertical );
+    QVERIFY( gh.isGridVisible() == true );
+    QVERIFY( gv.isGridVisible() == true );
+    gh.setGridVisible( false );
+    m_plane->setGridAttributes( Qt::Horizontal, gh );
+    QVERIFY( m_plane->hasOwnGridAttributes( Qt::Horizontal ) == true );
+    QVERIFY( m_plane->hasOwnGridAttributes( Qt::Vertical ) == false );
+    QVERIFY( m_plane->gridAttributes( Qt::Horizontal ).isGridVisible() == false );
+    QVERIFY( m_plane->gridAttributes( Qt::Vertical ).isGridVisible() == true );
+    gv.setGridVisible( false );
+    m_plane->setGridAttributes( Qt::Vertical, gv );
+    QVERIFY( m_plane->hasOwnGridAttributes( Qt::Horizontal ) == true );
+    QVERIFY( m_plane->hasOwnGridAttributes( Qt::Vertical ) == true );
+    QVERIFY( m_plane->gridAttributes( Qt::Horizontal ).isGridVisible() == false );
+    QVERIFY( m_plane->gridAttributes( Qt::Vertical ).isGridVisible() == false );
+    m_plane->resetGridAttributes( Qt::Horizontal );
+    m_plane->resetGridAttributes( Qt::Vertical );
+    QVERIFY( m_plane->gridAttributes( Qt::Horizontal ).isGridVisible() == true );
+    QVERIFY( m_plane->gridAttributes( Qt::Vertical ).isGridVisible() == true );
+    QVERIFY( m_plane->hasOwnGridAttributes( Qt::Horizontal ) == false );
+    QVERIFY( m_plane->hasOwnGridAttributes( Qt::Vertical ) == false );
+}
 
-};
+void TestCartesianPlanes::testAxesCalcModesSettings()
+{
+    QCOMPARE( m_plane->axesCalcModeX(), AbstractCoordinatePlane::Linear );
+    QCOMPARE( m_plane->axesCalcModeY(), AbstractCoordinatePlane::Linear );
+    m_plane->setAxesCalcModes( AbstractCoordinatePlane::Logarithmic );
+    QCOMPARE( m_plane->axesCalcModeX(), AbstractCoordinatePlane::Logarithmic );
+    QCOMPARE( m_plane->axesCalcModeY(), AbstractCoordinatePlane::Logarithmic );
+    m_plane->setAxesCalcModeX( AbstractCoordinatePlane::Linear );
+    QCOMPARE( m_plane->axesCalcModeX(), AbstractCoordinatePlane::Linear );
+    QCOMPARE( m_plane->axesCalcModeY(), AbstractCoordinatePlane::Logarithmic );
+    m_plane->setAxesCalcModeY( AbstractCoordinatePlane::Linear );
+    QCOMPARE( m_plane->axesCalcModeX(), AbstractCoordinatePlane::Linear );
+    QCOMPARE( m_plane->axesCalcModeY(), AbstractCoordinatePlane::Linear );
+}
+
 
 QTEST_MAIN(TestCartesianPlanes)
 
