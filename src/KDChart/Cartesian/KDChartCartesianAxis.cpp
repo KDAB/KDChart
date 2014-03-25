KDAB/KDChart

commit 2a7e03528b8826c191030d12376016e05ff645cd

Turn on clipping when zoomed in to avoid overpainting.
A less crude solution for the zoomed in case is possible, but requires
much more work.
I had removed (then unconditional) clipping in r45541, so this fixes
a regression.

git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@49761 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

src/KDChart/Cartesian/KDChartCartesianAxis.cpp
@@ -486,10 +486,19 @@ void CartesianAxis::paint( QPainter* painter )
     }
     PaintContext ctx;
     ctx.setPainter ( painter );
-    ctx.setCoordinatePlane( d->diagram()->coordinatePlane() );
+    AbstractCoordinatePlane *const plane = d->diagram()->coordinatePlane();
+    ctx.setCoordinatePlane( plane );
 
     ctx.setRectangle( QRectF( areaGeometry() ) );
     PainterSaver painterSaver( painter );
+
+    // enable clipping only when required due to zoom, because it slows down painting
+    // (the alternative to clipping when zoomed in requires much more work to paint just the right area)
+    const qreal zoomFactor = d->isVertical() ? plane->zoomFactorY() : plane->zoomFactorX();
+    if ( zoomFactor > 1.0 ) {
+        painter->setClipRegion( areaGeometry().adjusted( - d->amountOfLeftOverlap - 1, - d->amountOfTopOverlap - 1,
+                                                         d->amountOfRightOverlap + 1, d->amountOfBottomOverlap + 1 ) );
+    }
     paintCtx( &ctx );
 }
 

commit c7c2dd0de0af235835da2678d6dd0ba353926b75

Fix KDCHDEV-842, clipped axis title text
This was a regression introduced in r49761. The fix is simple: Never
clip the title text even when the rest of the axis is clipped.

git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@55231 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

src/KDChart/Cartesian/KDChartCartesianAxis.cpp
@@ -632,6 +632,7 @@ void CartesianAxis::Private::drawTitleText( QPainter* painter, CartesianCoordina
             break;
         }
         const PainterSaver painterSaver( painter );
+        painter->setClipping( false );
         painter->translate( point );
         titleItem.setGeometry( QRect( QPoint( -size.width() / 2, -size.height() / 2 ), size ) );
         titleItem.paint( painter );