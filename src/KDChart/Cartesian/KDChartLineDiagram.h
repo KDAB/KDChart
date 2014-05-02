f50be2ebb90ad1b405f3ee356f09104485c6959c
Sebastian Sauer <sebastian.sauer@kdab.com>
2014-05-02 22:29:34 +0000

    Remove khz comment in public API.
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@55820 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/KDChart/Cartesian/KDChartLineDiagram.h
+++ src/KDChart/Cartesian/KDChartLineDiagram.h
@@ -108,9 +108,6 @@ public:
     void setThreeDLineAttributes( const QModelIndex & index,
                                   const ThreeDLineAttributes & a );
 
-    //FIXME(khz): big TODO(khz): add a lot of reset...Attributes() methods to all
-    // appropriate places, for 2.1 (that is: after we have release 2.0.2)  :-)
-
     ThreeDLineAttributes threeDLineAttributes() const;
     ThreeDLineAttributes threeDLineAttributes( int column ) const;
     ThreeDLineAttributes threeDLineAttributes( const QModelIndex & index ) const;
