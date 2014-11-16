081b167ce92a28eaa4ab4863500f071c6b284d91
Milian Wolff <milian.wolff@kdab.com>
2014-11-16 13:28:10 +0000

    Update copyright notice to include 2014
    git-svn-id: svn+ssh://svn.kdab.com/home/SVN-klaralv/products/kdchart/trunk/kdchart@59080 c66179b2-ff0d-0410-8aef-1b1a91e65cfa

--- src/KDChart/KDChartModelDataCache_p.h
+++ src/KDChart/KDChartModelDataCache_p.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** Copyright (C) 2001-2013 Klaralvdalens Datakonsult AB.  All rights reserved.
+** Copyright (C) 2001-2014 Klaralvdalens Datakonsult AB.  All rights reserved.
 **
 ** This file is part of the KD Chart library.
 **
