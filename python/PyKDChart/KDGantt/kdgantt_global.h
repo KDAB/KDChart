d9ff792aaf93efece13bc6ef23f2fd8568f088eb
Allen Winter <allen.winter@kdab.com>
2019-11-27 13:27:56 -0500

    python/.../kdgantt_global.h - add license+copyright header

--- python/PyKDChart/KDGantt/kdgantt_global.h
+++ python/PyKDChart/KDGantt/kdgantt_global.h
@@ -1,3 +1,25 @@
+/****************************************************************************
+** Copyright (C) 2019 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com.
+** All rights reserved.
+**
+** This file is part of the KD Chart library.
+**
+** Licensees holding valid commercial KD Chart licenses may use this file in
+** accordance with the KD Chart Commercial License Agreement provided with
+** the Software.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 and version 3 as published by the
+** Free Software Foundation and appearing in the file LICENSE.GPL.txt included.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** Contact info@kdab.com if any conditions of this licensing are not
+** clear to you.
+**
+**********************************************************************/
+
 #define QT_ANNOTATE_ACCESS_SPECIFIER(a) __attribute__((annotate(#a)))
 
 #include <kdganttabstractgrid.h>
