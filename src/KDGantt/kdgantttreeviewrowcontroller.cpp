63bd0baaf152093bb14b3b4fa30c68daf3f00d7c
Allen Winter <allen.winter@kdab.com>
2021-07-19 13:19:51 -0400

    KDGantt - remove Qt4 support

--- src/KDGantt/kdgantttreeviewrowcontroller.cpp
+++ src/KDGantt/kdgantttreeviewrowcontroller.cpp
@@ -104,13 +104,7 @@ QModelIndex TreeViewRowController::indexAt( int height ) const
     QModelIndex idx = d->treeview->model()->index( 0, 0, d->treeview->rootIndex() );
     do {
         if ( y >= height ) break;
-#if QT_VERSION >= 0x040300
         y += d->treeview->rowHeight( idx );
-#else
-        // Since TreeViewRowController is NOT using uniform row height
-        // we can use this:
-        y += d->treeview->indexRowSizeHint( idx );
-#endif
         idx = d->treeview->indexBelow( idx );
     } while ( idx.isValid() );
     return d->proxy->mapFromSource( idx );
